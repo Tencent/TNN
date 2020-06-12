c07a4b1a8f98e228b665217c3f131cb014ddf8e4
ealinli <ealinli@tencent.com>
2020-06-12 16:30:19 +0800

    modify build.sh

--- tools/convert2tnn/build.sh
+++ tools/convert2tnn/build.sh
@@ -2,34 +2,39 @@ if [ ! -d "bin" ]; then
   mkdir bin
 fi
 
+if [ ! -d "temp" ]; then
+  mkdir temp
+fi
+
 function build_model_check() {
-	cd $1
+	cd $2
 
 	cmake ../../.. \
 		-DCMAKE_BUILD_TYPE=Release \
 		-DTNN_CPU_ENABLE:BOOL="ON" \
-		-DTNN_MODEL_CHECK_ENABLE:BOOL="ON" 
+		-DTNN_MODEL_CHECK_ENABLE:BOOL="ON" \
+		-DTNN_BUILD_SHARED="OFF"
 
 	make -j4
 
 	if [ -f "model_check" ]; then
-		rm -rf CMake*
-		rm -rf cmake*
-		rm -rf Make*
-		rm -rf source
-		rm -rf tools
+		mv model_check ../$1
+
+		cd ..
+
+		rm -rf $2
 		
-		cd ../../onnx2tnn/onnx-converter
+		cd ../onnx2tnn/onnx-converter
 		./build.sh
 
 		echo "Compiled successfully !"
 	else
 		cd ..
-		rm -rf $1
+		rm -rf $1 $2
 		echo "Compiled failed !!!"
 	fi
 }
 
-build_model_check bin
+build_model_check bin temp
 
 
