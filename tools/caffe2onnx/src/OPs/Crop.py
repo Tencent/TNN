1ed53a4ef3d9c0c4c5c615b1351470c208bc8e38
masterwan96 <64312349+masterwan96@users.noreply.github.com>
2020-06-29 14:21:05 +0800

    Add files via upload

---
+++ tools/caffe2onnx/src/OPs/Crop.py
@@ -0,0 +1,49 @@
+# Tencent is pleased to support the open source community by making TNN available.
+#
+# Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+# https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+import src.c2oObject as Node
+import numpy as np
+
+def get_crop_param(layer,input_shape):
+    axis: int = layer.crop_param.axis
+    crop_offset = layer.crop_param.offset
+    offset=[]
+    starts = []
+    axes = []
+    ends = []
+    for i in range(axis, len(input_shape[0])):
+        if((i-axis)>= len(crop_offset)):
+            offset.append(crop_offset[0])
+        else:
+            offset.append(crop_offset[i-axis])  
+        start = offset[i-axis]
+        end = start + input_shape[1][i]
+        axes.append(i)
+        starts.append(start)
+        ends.append(end)
+    print(starts)
+    print(ends)
+    print(axes)    
+    return starts, ends, axes
+
+
+def get_crop_output_shape(layer, input_shape):  
+    print(input_shape[1])
+    return [input_shape[1]]
+
+
+def create_crop_node(layer, node_name, input_name, output_name, input_shape):
+    output_shape = get_crop_output_shape(layer, input_shape)
+    node = Node.c2oNode(layer, node_name, "Slice", input_name, output_name, input_shape, output_shape)
+    return node
