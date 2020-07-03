9ea363c68c4a3564486aacaf1f011df32245d0d0
quinnrong94 <quinnrong@tencent.com>
2020-07-03 19:15:15 +0800

    [CI] fix build ios path error

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -3,7 +3,7 @@ export DEVELOPER_DIR=/Applications/Xcode.app/Contents/Developer
 
 if [ -z $TNN_ROOT_PATH ]
 then
-  TNN_ROOT_PATH=$(dirname $(readlink -f $0))/..
+  TNN_ROOT_PATH=$(cd "$(dirname "$0")";pwd)/..
 fi
 
 TNN_BUILD_PATH=$TNN_ROOT_PATH/platforms/ios
