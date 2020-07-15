f5d836c253756d688f2273d2647d061bcf87f49a
masterwan96 <64312349+masterwan96@users.noreply.github.com>
2020-07-15 16:48:23 +0800

    Update __init__.py
    Add MeanVarianceNorm OP

--- tools/caffe2onnx/src/OPs/__init__.py
+++ tools/caffe2onnx/src/OPs/__init__.py
@@ -31,4 +31,4 @@ from src.OPs.LpNormalization import create_Lp_Normalization
 from src.OPs.Power import get_power_param, create_power_node
 from src.OPs.Add import create_add_node
 from src.OPs.Tanh import createTanh
-
+from src.OPs.MeanVarianceNorm import *
