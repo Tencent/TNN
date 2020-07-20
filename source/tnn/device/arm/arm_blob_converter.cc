90e84b77bdbbf74953116bce65b78613ef36b7a3
Xiao Dong <devandong@tencent.com>
2020-07-20 16:14:51 +0800

    fix wrong stepsize in RGBA reverse kernel

--- source/tnn/device/arm/arm_blob_converter.cc
+++ source/tnn/device/arm/arm_blob_converter.cc
@@ -367,7 +367,7 @@ void RGBAChannelReverse(uint8_t *ptr, int channel, int hw) {
     }
 #endif
     for (; i < hw; i++) {
-        std::swap(ptr[i * channel], ptr[i * channel + 2]);
+        std::swap(ptr[i * 4], ptr[i * 4 + 2]);
     }
 }
 
