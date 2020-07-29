8cd17707f21823a78f53027528a1b97d294b2990
fudawan <fudawan@tencent.com>
2020-07-29 16:23:29 +0800

    add caffe2onnx.md

--- tools/caffe2onnx/src/OPs/InstanceNorm.py
+++ tools/caffe2onnx/src/OPs/InstanceNorm.py
@@ -1,23 +1,24 @@
 import src.c2oObject as Node
 import numpy as np
 
-def get_InstanceNorm_param(layer,input_shape):
-    scale=[]
-    bias=[]
+
+def get_InstanceNorm_param(layer, input_shape):
+    scale = []
+    bias = []
     for i in range(input_shape[0][1]):
         scale.append(1)
         bias.append(0)
     return scale, bias
 
+
 def create_InstanceNorm_attributes(layer):
     epsilon: float = layer.mvn_param.eps
     if not epsilon:
-       epsilon = 1e-05
+        epsilon = 1e-05
 
-    attributes = {
-        'epsilon': epsilon
-    }
+    attributes = {"epsilon": epsilon}
     return attributes
+
 
 def get_InstanceNorm_output_shape(input_shape):
     output_shape = input_shape
@@ -27,8 +28,6 @@ def get_InstanceNorm_output_shape(input_shape):
 def create_InstanceNorm_op(layer, node_name, input_name, output_name, input_shape):
     output_shape = get_InstanceNorm_output_shape(input_shape)
     attributes = create_InstanceNorm_attributes(layer)
-    # node = Node.c2oNode(layer, node_name, "InstanceNormalization", input_name, output_name,
-    #                     input_shape, output_shape)
     node = Node.c2oNode(layer, node_name, "InstanceNormalization",
                         input_name, output_name,
                         input_shape,output_shape,attributes)
