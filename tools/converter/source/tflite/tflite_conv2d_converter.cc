b583b93b6d704352f85d87ce4456afe5fa9557c3
lucasktian <lucasktian@tencent.com>
2020-07-29 13:08:12 +0800

    [TFLITE][UPD] 1. refactor tflite_conv2d_converter; 2. delete tflite_depthwise_conv2d_converter.cc;

--- tools/converter/source/tflite/tflite_conv2d_converter.cc
+++ tools/converter/source/tflite/tflite_conv2d_converter.cc
@@ -34,11 +34,13 @@ TNN_NS::Status TFLiteConv2DConverter::exec(TNN_NS::NetStructure& net_structure, 
                                            bool quantized_model) {
     TNN_NS::ConvLayerParam* param = new TNN_NS::ConvLayerParam;
     auto cur_layer                = net_structure.layers.back();
-    cur_layer->param = std::shared_ptr<TNN_NS::LayerParam>(param);
+    cur_layer->param              = std::shared_ptr<TNN_NS::LayerParam>(param);
+    param->name                   = cur_layer->name;
+    param->type                   = cur_layer->type_str;
+    param->quantized              = false;
     // 3|2 inputs: input tensor, weight, (bias)
     const int input_size = tf_lite_operator->inputs.size();
     ASSERT(input_size == 2 || input_size == 3);
-    const auto& conv_opt = tf_lite_operator->builtin_options.AsConv2DOptions();
     // weight index
     const int weight_index    = tf_lite_operator->inputs[1];
     const auto& weight_tensor = tf_lite_tensors[weight_index];
@@ -50,38 +52,34 @@ TNN_NS::Status TFLiteConv2DConverter::exec(TNN_NS::NetStructure& net_structure, 
     const int kw          = weight_shape[2];
     const int ci          = weight_shape[3];
     const int weight_size = co * kh * kw * ci;
-    if (quantized_model) {
-        // TODO
-    } else {
-        param->name           = cur_layer->name;
-        param->type           = cur_layer->type_str;
-        param->quantized      = false;
+    auto tf_lite_op_type  = tf_lite_op_set[tf_lite_operator->opcode_index]->builtin_code;
+    if (tf_lite_op_type == tflite::BuiltinOperator_CONV_2D) {
+        const auto option     = tf_lite_operator->builtin_options.AsConv2DOptions();
         param->input_channel  = ci;
         param->output_channel = co;
         param->kernels.push_back(kw);
         param->kernels.push_back(kh);
-        param->strides.push_back(conv_opt->stride_w);
-        param->strides.push_back(conv_opt->stride_h);
-        param->dialations.push_back(conv_opt->dilation_w_factor);
-        param->dialations.push_back(conv_opt->dilation_h_factor);
+        param->strides.push_back(option->stride_w);
+        param->strides.push_back(option->stride_h);
+        param->dialations.push_back(option->dilation_w_factor);
+        param->dialations.push_back(option->dilation_h_factor);
         param->group    = 1;
         param->pad_type = 0;
-        if (conv_opt->padding == tflite::Padding_VALID) {
+        if (option->padding == tflite::Padding_VALID) {
             // tensorflow pad valid
             param->pad_type = -1;
             param->pads.push_back(0);
             param->pads.push_back(0);
             param->pads.push_back(0);
             param->pads.push_back(0);
-        } else if (conv_opt->padding == tflite::Padding_SAME) {
+        } else if (option->padding == tflite::Padding_SAME) {
             param->pad_type = 0;
             param->pads.push_back(0);
             param->pads.push_back(0);
             param->pads.push_back(0);
             param->pads.push_back(0);
         }
-
-        const auto activation = conv_opt->fused_activation_function;
+        const auto activation = option->fused_activation_function;
         if (activation == tflite::ActivationFunctionType_RELU) {
             param->activation_type = TNN_NS::ActivationType_ReLU;
         } else if (activation == tflite::ActivationFunctionType_RELU6) {
@@ -90,6 +88,51 @@ TNN_NS::Status TFLiteConv2DConverter::exec(TNN_NS::NetStructure& net_structure, 
             LOGE("TNN Conv2D do not Support fused_activation_function\n");
             return TNN_NS::TNNERR_CONVERT_UNSUPPORT_LAYER;
         }
+    } else if (tf_lite_op_type == tflite::BuiltinOperator_DEPTHWISE_CONV_2D) {
+        const auto depthwise_option = tf_lite_operator->builtin_options.AsDepthwiseConv2DOptions();
+        assert(co == 1);
+        const int cm          = co;
+        param->input_channel  = ci;
+        param->output_channel = ci * cm;
+        param->kernels.push_back(kw);
+        param->kernels.push_back(kh);
+        param->strides.push_back(depthwise_option->stride_w);
+        param->strides.push_back(depthwise_option->stride_h);
+        param->dialations.push_back(depthwise_option->dilation_w_factor);
+        param->dialations.push_back(depthwise_option->dilation_h_factor);
+        param->group    = ci;
+        param->pad_type = 0;
+        if (depthwise_option->padding == tflite::Padding_VALID) {
+            // tensorflow pad valid
+            param->pad_type = -1;
+            param->pads.push_back(0);
+            param->pads.push_back(0);
+            param->pads.push_back(0);
+            param->pads.push_back(0);
+        } else if (depthwise_option->padding == tflite::Padding_SAME) {
+            param->pad_type = 0;
+            param->pads.push_back(0);
+            param->pads.push_back(0);
+            param->pads.push_back(0);
+            param->pads.push_back(0);
+        }
+        const auto activation = depthwise_option->fused_activation_function;
+        if (activation == tflite::ActivationFunctionType_RELU) {
+            param->activation_type = TNN_NS::ActivationType_ReLU;
+        } else if (activation == tflite::ActivationFunctionType_RELU6) {
+            param->activation_type = TNN_NS::ActivationType_ReLU6;
+        } else if (activation > tflite::ActivationFunctionType_NONE) {
+            LOGE("TNN Conv2D do not Support fused_activation_function\n");
+            return TNN_NS::TNNERR_CONVERT_UNSUPPORT_LAYER;
+        }
+    } else {
+        LOGE("TNN Conv2D do not Support operator\n");
+        return TNN_NS::TNNERR_CONVERT_UNSUPPORT_LAYER;
+    }
+
+    if (quantized_model) {
+        // TODO
+    } else {
         // weight
         auto layer_resource  = new TNN_NS::ConvLayerResource;
         layer_resource->name = cur_layer->name;
@@ -101,12 +144,12 @@ TNN_NS::Status TFLiteConv2DConverter::exec(TNN_NS::NetStructure& net_structure, 
         layer_resource->filter_handle = filter_handle;
         // bias
         if (input_size == 3) {
-            param->bias = 1;
-            TNN_NS::RawBuffer bias_handle = TNN_NS::RawBuffer(co * sizeof(float));
-            const auto& bias_tensor = tf_lite_tensors[tf_lite_operator->inputs[2]];
+            param->bias                   = 1;
+            TNN_NS::RawBuffer bias_handle = TNN_NS::RawBuffer(param->output_channel * sizeof(float));
+            const auto& bias_tensor       = tf_lite_tensors[tf_lite_operator->inputs[2]];
             auto bias_data_ptr = reinterpret_cast<const float*>(tf_lite_model_buffer[bias_tensor->buffer]->data.data());
-            ::memcpy(bias_handle.force_to<float*>(), bias_data_ptr, sizeof(float) * co);
-            layer_resource->bias_handle                = bias_handle;
+            ::memcpy(bias_handle.force_to<float*>(), bias_data_ptr, param->output_channel * sizeof(float));
+            layer_resource->bias_handle = bias_handle;
         }
         net_resource.resource_map[cur_layer->name] = std::shared_ptr<TNN_NS::LayerResource>(layer_resource);
     }
@@ -120,5 +163,6 @@ TNN_NS::Status TFLiteConv2DConverter::exec(TNN_NS::NetStructure& net_structure, 
 }
 using namespace tflite;
 REGISTER_CONVERTER(Conv2D, BuiltinOperator_CONV_2D);
+REGISTER_CONVERTER(Conv2D, BuiltinOperator_DEPTHWISE_CONV_2D);
 
 }  // namespace TNN_CONVERTER
