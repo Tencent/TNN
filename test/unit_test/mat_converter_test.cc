aeec9731e470c950a9a156a8d8e08b43037a1761
ZYJESJ <2673274393@qq.com>
2020-08-24 11:53:09 +0800

    test resize

--- test/unit_test/mat_converter_test.cc
+++ test/unit_test/mat_converter_test.cc
@@ -66,7 +66,7 @@ void MatConverterTest::TearDownTestCase() {
     delete device_context_;
 }
 
-int MatConverterTest::CreateTestData(int batch, int channel, int input_size, MatType mat_type) {
+int MatConverterTest::CreateTestData(int batch, int channel, int input_size, MatType mat_type, bool resize_flag) {
     int mat_channel;
     if (mat_type == N8UC4) {
         mat_channel = 4;
@@ -75,7 +75,10 @@ int MatConverterTest::CreateTestData(int batch, int channel, int input_size, Mat
     }
 
     int in_size             = batch * mat_channel * input_size * input_size;
-    out_size_               = in_size;
+    if(resize_flag)
+        out_size_ = batch * mat_channel * 224 * 224;
+    else
+        out_size_               = in_size;
     mat_in_data_            = nullptr;
     mat_out_ref_data_       = nullptr;
     mat_out_dev_data_       = nullptr;
@@ -107,9 +110,9 @@ int MatConverterTest::DestroyTestData()
 INSTANTIATE_TEST_SUITE_P(MatConverterTest, MatConverterTest,
                          ::testing::Combine(
                             // batch
-                            testing::Values(1, 2),
+                            testing::Values(1),
                             // channel
-                            testing::Values(1, 3, 4),
+                            testing::Values(4),
                             // input size
                             testing::Values(3, 10, 20, 128),
                             // mat type
@@ -121,6 +124,7 @@ INSTANTIATE_TEST_SUITE_P(MatConverterTest, MatConverterTest,
                                 // MatConverterTestParam(MatConverterType::Copy),
                                 // Resize
                                 // MatConverterTestParam(MatConverterType::Resize, 0.1, 0.1, INTERP_TYPE_LINEAR),
+                                // MatConverterTestParam(MatConverterType::Resize, 0.1, 0.1, INTERP_TYPE_LINEAR)
                                 // Crop
                                 // MatConverterTestParam(MatConverterType::Crop, 0, 0, 10, 10),
                                 // WarpAffine
@@ -137,7 +141,8 @@ INSTANTIATE_TEST_SUITE_P(MatConverterTest, MatConverterTest,
                                 MatConverterTestParam(MatConverterType::WarpAffine, 2, 1, 100, 3, 7, 50,
                                                       INTERP_TYPE_LINEAR, BORDER_TYPE_CONSTANT, FLT_MIN),
                                 MatConverterTestParam(MatConverterType::WarpAffine, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
-                                                      INTERP_TYPE_LINEAR, BORDER_TYPE_CONSTANT, FLT_MIN))
+                                                      INTERP_TYPE_LINEAR, BORDER_TYPE_CONSTANT, FLT_MIN)
+                                                      )
                             ));
 
 TEST_P(MatConverterTest, MatConverterTest) {
@@ -149,8 +154,8 @@ TEST_P(MatConverterTest, MatConverterTest) {
     MatConverterType mat_converter_type             = mat_converter_test_param.mat_converter_type;
 
     DeviceType device_type  = ConvertDeviceType(FLAGS_dt);
-    // warp affine only support N8UC4 on OpenCL for now
-    if (device_type == DEVICE_OPENCL && mat_converter_type == MatConverterType::WarpAffine && !(mat_type == N8UC4))
+    // warp affine/resize only support N8UC4 on OpenCL for now
+    if (device_type == DEVICE_OPENCL && (mat_converter_type == MatConverterType::WarpAffine || mat_converter_type == MatConverterType::Resize) && !(mat_type == N8UC4))
     {
         GTEST_SKIP();
     }
@@ -160,19 +165,42 @@ TEST_P(MatConverterTest, MatConverterTest) {
         GTEST_SKIP();
     }
 
-    int rtn = CreateTestData(batch, channel, input_size, mat_type);
+    int output_size = 224;
+    DimsVector dims         = {batch, channel, input_size, input_size};
+    DimsVector dims_out = {batch, channel, output_size, output_size};
+    bool resize_flag = false;
+    if (mat_converter_type == MatConverterType::Resize){
+        resize_flag = true;
+        DimsVector dims_out = {batch, channel, output_size, output_size};
+    } else {
+        DimsVector dims_out = {batch, channel, input_size, input_size};
+        resize_flag = false;
+    }
+
+    int rtn = CreateTestData(batch, channel, input_size, mat_type, resize_flag);
     EXPECT_EQ(rtn, 0);
 
 
-    DimsVector dims         = {batch, channel, input_size, input_size};
+
     Mat cpu_in_mat          = Mat(DEVICE_NAIVE, mat_type, dims, mat_in_data_);
-    Mat cpu_ref_mat         = Mat(DEVICE_NAIVE, mat_type, dims, mat_out_ref_data_);
-    Mat cpu_out_mat         = Mat(DEVICE_NAIVE, mat_type, dims, mat_out_dev_data_);
-    Mat device_mat          = Mat(device_type, mat_type, dims);
+    Mat cpu_ref_mat         = Mat(DEVICE_NAIVE, mat_type, dims_out, mat_out_ref_data_);
+    Mat cpu_out_mat         = Mat(DEVICE_NAIVE, mat_type, dims_out, mat_out_dev_data_);
+    Mat device_mat          = Mat(device_type, mat_type, dims_out);
     Mat device_in_mat       = Mat(device_type, mat_type, dims);
     int cmp_result          = 0;
     void* device_command_queue;
     device_context_->GetCommandQueue(&device_command_queue);
+
+    //arm compare
+
+    int out_size = batch * channel * output_size * output_size;
+    void* mat_in_data_arm = nullptr;
+    void* mat_out_arm_data = nullptr;
+    mat_in_data_arm =  malloc(batch * channel * input_size * input_size * sizeof(uint8_t));
+    mat_out_arm_data = malloc(out_size * sizeof(uint8_t));
+    Mat arm_device_in(DEVICE_ARM, N8UC4, dims, mat_in_data_arm);
+    Mat arm_device_out(DEVICE_ARM, N8UC4, dims_out, mat_out_arm_data);
+
     switch (mat_converter_type)
     {
         case MatConverterType::Copy:
@@ -197,8 +225,55 @@ TEST_P(MatConverterTest, MatConverterTest) {
         }
         case MatConverterType::Resize:
         {
-            LOGE("mat converter resize test start\n");
-            LOGE("mat converter resize test end\n");
+            // Mat *src, *dst, *dst_ref;
+            // src         = &cpu_in_mat;
+            // dst         = &device_mat;
+            // dst_ref     = &cpu_ref_mat;
+
+            Mat *src, *src_arm, *dst_arm, *dst;
+            src         = &cpu_in_mat;
+            memcpy(mat_in_data_arm, mat_in_data_, batch * channel * input_size * input_size);
+            src_arm = &arm_device_in;
+            dst_arm = &arm_device_out;
+            dst         = &device_mat;
+            MatConverter arm_converter(src_arm,dst_arm);
+
+            LOGE("resizeon host start\n");
+            tnn::Status status = arm_converter.Resize(arm_device_in, arm_device_out, mat_converter_test_param.resize_param, NULL);
+            if (status == TNN_OK)
+            {
+                LOGE("resize test start\n");
+            }
+            else
+            {
+                LOGE("resize test failed\n");
+                FAIL();
+            }
+
+            MatConverter device_converter(src, dst);
+            LOGE("resize on device start\n");
+            status = device_converter.Copy(cpu_in_mat, device_in_mat,
+                                           device_command_queue);
+
+            status = device_converter.Resize(device_in_mat, device_mat,
+                                                 mat_converter_test_param.resize_param,
+                                                 device_command_queue);
+            if (status == TNN_OK)
+            {
+                LOGE("resize on device done\n");
+            }
+            else
+            {
+                LOGE("resize on device failed\n");
+                FAIL();
+            }
+
+            device_converter.Copy(device_mat, cpu_out_mat, device_command_queue);
+
+            cmp_result |= CompareData(static_cast<uint8_t*>(mat_out_arm_data), static_cast<uint8_t*>(mat_out_dev_data_),
+                                      channel, channel, out_size);
+
+            EXPECT_EQ(0, cmp_result);
             break;
         }
         case MatConverterType::Crop:
@@ -233,7 +308,7 @@ TEST_P(MatConverterTest, MatConverterTest) {
             LOGE("warp affine on device start\n");
             status = device_converter.Copy(cpu_in_mat, device_in_mat,
                                            device_command_queue);
-
+            LOGE("warp affine on device start1\n");
             status = device_converter.WarpAffine(device_in_mat, device_mat,
                                                  mat_converter_test_param.warp_affine_param,
                                                  device_command_queue);
