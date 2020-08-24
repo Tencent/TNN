09e871d01db226db0e3c137d847e3b3c1ded91e6
ealinli <1627180283@github.com>
2020-08-24 23:47:51 +0800

    [CONVERTER][UPD] improve the logic of RoiAlign's fusion code

--- tools/onnx2tnn/src/core/onnx_fuse/onnx2tnn_fuse_roi_align.cc
+++ tools/onnx2tnn/src/core/onnx_fuse/onnx2tnn_fuse_roi_align.cc
@@ -12,16 +12,14 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include <math.h>
-
 #include "objseri.h"
 #include "onnx2tnn.h"
 
-int Onnx2TNN::FuseRoiAlign(onnx::GraphProto* mutable_graph,
-                           std::vector<IndexNode>& index_nodes,
-                           std::map<std::string, onnx::TensorProto>& weights,
-                           std::map<std::string, int>& node_reference,
-                           std::set<std::string>& blob_names) {
+int Onnx2TNN::FuseRoiAlign(onnx::GraphProto *mutable_graph,
+                           std::vector<IndexNode> &index_nodes,
+                           std::map<std::string, onnx::TensorProto> &weights,
+                           std::map<std::string, int> &node_reference,
+                           std::set<std::string> &blob_names) {
     auto const node_count = index_nodes.size();
 
     // RoiAlign <= Gather - Squeeze - Cast -Gather -RoiAlign
@@ -51,12 +49,23 @@ int Onnx2TNN::FuseRoiAlign(onnx::GraphProto* mutable_graph,
             auto reshape = index_nodes[i + 3].node;
             reshape->set_op_type("Reshape");
             reshape->set_input(0, node->output(0));
+            auto tensor = onnx::TensorProto(weights[reshape->input(1)]);
+            auto tensor_name = reshape->input(1) + "_shape";
+
+            int64_t *tensor_data = (int64_t *) get_tensor_proto_mutable_data(tensor);
             std::vector<int> shape_data = {1, -1, 5, 1};
-            onnx::TensorProto shape;
-            shape.set_name(reshape->input(1));
-            shape.add_dims(4);
-            shape.mutable_raw_data()->assign(reinterpret_cast<const char *>(shape_data.data()), shape_data.size() * sizeof(int));
-            weights[reshape->input(1)] = std::move(shape);
+            for (int i = 0; i < 4; i++) {
+                tensor_data[i] = shape_data[i];
+            }
+
+            weights[tensor_name] = tensor;
+            reshape->set_input(1, tensor_name);
+
+            auto roi_align_input = roi_align->input(0);
+            roi_align->clear_input();
+            roi_align->add_input(roi_align_input);
+            roi_align->add_input(reshape->output(0));
+
             i += 5;
 
         } while (0);
