869643370e5b56a1d1b5ddfdc51f765efdca02f1
quinnrong94 <quinnrong@tencent.com>
2020-09-04 18:04:10 +0800

    [TEST] add int8 test for add op and inner product op

--- test/unit_test/layer_test/test_innerproduct_int8_layer.cc
+++ test/unit_test/layer_test/test_innerproduct_int8_layer.cc
@@ -25,14 +25,14 @@ class InnerProductInt8LayerTest : public LayerTest,
 INSTANTIATE_TEST_SUITE_P(LayerTest, InnerProductInt8LayerTest,
                          ::testing::Combine(testing::Values(1), testing::Values(3, 4, 8, 9, 16),
                                             // output channel
-                                            testing::Values(1)));
+                                            testing::Values(1, 4, 8, 16, 32)));
 
 TEST_P(InnerProductInt8LayerTest, InnerProductLayer) {
     // get param
     int batch          = std::get<0>(GetParam());
     int input_channel  = std::get<1>(GetParam());
-    int input_size     = std::get<2>(GetParam());
-    int output_channel = input_channel;
+    int output_channel = std::get<2>(GetParam());
+    int input_size     = 1;
     DeviceType dev     = ConvertDeviceType(FLAGS_dt);
     if (DEVICE_ARM != dev) {
         GTEST_SKIP();
