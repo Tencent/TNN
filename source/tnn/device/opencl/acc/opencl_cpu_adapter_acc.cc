33f184a6baac7ac3e36adce9a260de082a954dfe
quinnrong94 <quinnrong@tencent.com>
2020-09-25 14:02:26 +0800

    [CodeCC] fix UNINIT_CTOR

--- source/tnn/device/opencl/acc/opencl_cpu_adapter_acc.cc
+++ source/tnn/device/opencl/acc/opencl_cpu_adapter_acc.cc
@@ -23,6 +23,7 @@ namespace TNN_NS {
 OpenCLCpuAdapterAcc::OpenCLCpuAdapterAcc(LayerType impl_layer_type) {
     impl_layer_type_ = impl_layer_type;
     cpu_adapter_acc_ = NULL;
+    impl_device_type_ = DEVICE_ARM;
     impl_device_context_ = NULL;
     DeviceType device_list[2] = {DEVICE_ARM, DEVICE_X86};
     for(auto device_type : device_list) {
