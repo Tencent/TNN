193a66083d76815b2f327e51e9d52c100985da31
lnmdlong <lnmdlong@hotmail.com>
2020-10-26 17:21:15 +0800

    [OPENCL] fix chinese comments

--- source/tnn/device/opencl/opencl_utils.cc
+++ source/tnn/device/opencl/opencl_utils.cc
@@ -220,7 +220,7 @@ Status AdjustBuildOptionForFp32(std::set<std::string>& build_options)
     char sdk[128] = "0";
     __system_property_get("ro.build.version.sdk", sdk);
     int sdk_version = atoi(sdk);
-    // Android 7.1之前版本 fp16 exp 部分机型上的速度有问题，改用fp32版本的kernel
+    // On Android 7.1, support for exp FP16 is limited on some devices
     force_fp32 = (sdk_version <= 25);
 #elif (defined __ANDROID_API__) && (__ANDROID_API__ < 21)
     force_fp32 = true;
