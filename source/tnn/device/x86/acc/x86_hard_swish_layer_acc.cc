ae659df1ed8a4d89d81c970cf6137d06b38a0895
Maosquerade <shenpeng.wang@qq.com>
2020-10-28 16:57:54 +0800

    [X86] Add HardSwish layer acc

--- source/tnn/device/x86/acc/x86_hard_swish_layer_acc.cc
+++ source/tnn/device/x86/acc/x86_hard_swish_layer_acc.cc
@@ -12,40 +12,134 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include "tnn/device/x86/acc/x86_unary_layer_acc.h"
+#include "tnn/device/x86/acc/x86_layer_acc.h"
+#include "tnn/device/x86/x86_device.h"
+#include "immintrin.h"
 #include <math.h>
+#include <iostream>
 
 namespace TNN_NS {
 
-typedef struct x86_hardswish_layer_acc : x86_unary_operator {
-public:
-    Status Init(LayerParam *param) {
-        auto layer_param = dynamic_cast<HardSwishLayerParam *>(param);
-        if (!layer_param) {
-            LOGE("Error: clip layer param is nil\n");
-            return Status(TNNERR_MODEL_ERR, "Error, hard swish layer param is nil");
+DECLARE_X86_ACC(HardSwish, X86_HARDSWISH_OP);
+
+Status X86HardSwishLayerAcc::Reshape(const std::vector<Blob *> &inputs, const std::vector<Blob *> &outputs) {
+    return TNN_OK;
+}
+
+Status X86HardSwishLayerAcc::Forward(const std::vector<Blob *> &inputs, const std::vector<Blob *> &outputs) {
+    
+    auto param = dynamic_cast<HardSwishLayerParam *>(param_);
+    if (!param) {
+        LOGE("Error: HardSwishLayerParam is nil\n");
+        return Status(TNNERR_MODEL_ERR, "Error: HardSwishLayerParam is nil");
+    }
+
+    Blob *input_blob0, *input_blob1;
+    if (inputs.size() == 1) {
+        input_blob0 = inputs[0];
+        input_blob1 = inputs[0];
+    } else {
+        input_blob0 = inputs[0];
+        input_blob1 = inputs[1];
+    }
+
+    const float alpha = param->alpha;
+    const float beta  = param->beta;
+    const float minV  = - beta / alpha;
+    const float maxV  = (1.0f - beta) / alpha;
+
+    auto input_dim0  = input_blob0->GetBlobDesc().dims;
+    auto input_dim1  = input_blob1->GetBlobDesc().dims;
+
+    const int batch = input_dim0[0];
+    const int channel = input_dim0[1];
+    const int channel_size = input_dim0[2] * input_dim0[3];
+
+    auto input_ptr0 = reinterpret_cast<float *>(input_blob0->GetHandle().base);
+    auto input_ptr1 = reinterpret_cast<float *>(input_blob1->GetHandle().base);
+
+    auto output_ptr = reinterpret_cast<float *>(outputs[0]->GetHandle().base);
+
+#ifdef __AVX2__
+    register __m256 alpha_, beta_, zero_, one_, tmp00_, tmp01_, tmp02_, tmp03_, tmp10_, tmp11_, tmp12_, tmp13_;
+    alpha_ = _mm256_set1_ps(alpha);
+    beta_  = _mm256_set1_ps(beta);
+    zero_  = _mm256_setzero_ps();
+    one_   = _mm256_set1_ps(1.f);
+
+    int total_size = batch * channel * channel_size;
+    int tail = total_size - total_size % 8;
+    int part_tail = tail / 4;
+    const int offset1 = part_tail, offset2 = part_tail * 2, offset3 = part_tail * 3;
+
+    if (input_dim0[2] == input_dim1[2]) {
+        for (int index = 0; index < tail; index += 8) {
+            tmp00_ = _mm256_loadu_ps(input_ptr0 + index);
+            tmp10_ = _mm256_loadu_ps(input_ptr1 + index);
+            tmp10_ = _mm256_fmadd_ps(tmp10_, alpha_, beta_);
+            tmp10_ = _mm256_min_ps(tmp10_, one_);
+            tmp10_ = _mm256_max_ps(tmp10_, zero_);
+            tmp10_ = _mm256_mul_ps(tmp10_, tmp00_);
+            _mm256_storeu_ps(output_ptr + index, tmp10_);
         }
-        alpha_ = layer_param->alpha;
-        beta_  = layer_param->beta;
-        return TNN_OK;
+
+        // build mask
+        float mask[8] = {0.f};
+        for (int i = 0; i < total_size % 8; i++) mask[i] = -1.f;
+        __m256i mask_ = _mm256_loadu_si256((__m256i*)mask);
+
+        tmp00_ = _mm256_maskload_ps(input_ptr0 + tail, mask_);
+        tmp10_ = _mm256_maskload_ps(input_ptr1 + tail, mask_);
+
+        tmp10_ = _mm256_fmadd_ps(tmp10_, alpha_, beta_);
+        tmp10_ = _mm256_min_ps(tmp10_, one_);
+        tmp10_ = _mm256_max_ps(tmp10_, zero_);
+        tmp00_ = _mm256_mul_ps(tmp00_, tmp10_);
+        _mm256_maskstore_ps(output_ptr + tail, mask_, tmp00_);
+    } else {
+        tail = channel_size - channel_size % 8;
+        float mask[8] = {0.f};
+        for (int i = 0; i < channel_size % 8; i++) mask[i] = -1.f;
+        __m256i mask_ = _mm256_loadu_si256((__m256i*)mask);
+
+        for (int b = 0; b < batch; b++) {
+            for (int c = 0; c < channel; c++) {
+                float* input_data0 = input_ptr0 + (b * channel + c) * channel_size;
+                float* input_data1 = input_ptr1 + (b * channel + c);
+                float* output_data = output_ptr + (b * channel + c) * channel_size;
+                float tmp = (*input_data1) * alpha + beta;
+                tmp = std::max(std::min(tmp, 1.f), 0.f);
+                tmp10_ = _mm256_set1_ps(tmp);
+                for (int index = 0; index < tail; index += 8) {
+                    tmp00_ = _mm256_loadu_ps(input_data0 + index);
+                    tmp00_ = _mm256_mul_ps(tmp00_, tmp10_);
+                    _mm256_storeu_ps(output_data + index, tmp00_);
+                }
+                tmp00_ = _mm256_maskload_ps(input_data0 + tail, mask_);
+                tmp00_ = _mm256_mul_ps(tmp00_, tmp10_);
+                _mm256_maskstore_ps(output_data + tail, mask_, tmp00_);
+            }
+        }
     }
 
-    virtual float operator()(const float v) {
-        float tmp = v * alpha_ + beta_;
-        tmp = std::min(1.0f, tmp);
-        tmp = std::max(0.0f, tmp);
-        tmp *= v;
-        return tmp;
+#else
+    for (int b = 0; b < batch; b++) {
+        for (int c = 0; c < channel; c++) {
+            auto input_data0 = input_ptr0 + (b * channel + c) * channel_size;
+            auto input_data1 = input_ptr1 + (b * channel + c) * channel_size;
+            auto output_data = output_ptr + (b * channel + c) * channel_size;
+            for (int index = 0; index < channel_size; index++) {
+                float tmp = input_data1[index] * alpha + beta;
+                tmp = std::min(tmp, 1.f);
+                tmp = std::max(tmp, 0.f);
+                output_data[index] = input_data0[index] * tmp;
+            }
+        }
     }
-private:
-    float min_ = 0.f;
-    float max_ = 0.f;
-    float alpha_ = 1.f;
-    float beta_ = 0.f;
-} X86_HARDSWISH_OP;
+#endif
+    return TNN_OK;
+}
 
-DECLARE_X86_UNARY_ACC(HardSwish, X86_HARDSWISH_OP);
-
-// REGISTER_X86_ACC(HardSwish, LAYER_HARDSWISH);
+REGISTER_X86_ACC(HardSwish, LAYER_HARDSWISH);
 
 }   // namespace TNN_NS
