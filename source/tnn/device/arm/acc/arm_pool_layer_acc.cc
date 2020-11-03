60c1b643f8ad635e3ff7a180477c665406688bc5
quinnrong94 <quinnrong@tencent.com>
2020-11-03 12:21:16 +0800

    [ARM] register fp16 precision for pooling layer

--- source/tnn/device/arm/acc/arm_pool_layer_acc.cc
+++ source/tnn/device/arm/acc/arm_pool_layer_acc.cc
@@ -138,5 +138,6 @@ Status ArmPoolingLayerAcc::DoForward(const std::vector<Blob *> &inputs, const st
 }
 
 REGISTER_ARM_ACC(Pooling, LAYER_POOLING)
+REGISTER_ARM_PRECISION_FP16(LAYER_POOLING)
 
 }  // namespace TNN_NS
