043cb45bfbbb197d6f6a9e7703b42def48d88b4a
ealinli <ealinli@tencent.com>
2020-12-21 18:05:36 +0800

    [CONVERT][UPD] remove numpy from fix_tnn_output.py

--- tools/convert2tnn/utils/fix_tnn_output.py
+++ tools/convert2tnn/utils/fix_tnn_output.py
@@ -11,27 +11,25 @@
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
-import numpy as np
 
-
-def get_output_info(tnnproto: np.ndarray) -> list:
+def get_output_info(tnnproto: list) -> list:
     output_info = tnnproto[3]
     output_name = output_info.split("\"")[1]
     output_name = output_name.split(" ")[:-1]
     return output_name
 
 
-def find_target_layer(content: np.ndarray, target_name: str):
+def find_target_layer(content: list, target_name: str):
     for idx, layer in enumerate(content):
         if target_name in layer:
             name = layer.split(" ")[1]
             return idx, name
 
-    raise Exception("Conversion failed");
+    raise Exception("Conversion failed")
 
 
 def generate_transpose(input_name: str, output_name: str) -> str:
-    return "\"Permute {} 1 1 {} {} 4 0 3 1 2 ,\"" .format(output_name, input_name, output_name)
+    return "\"Permute {} 1 1 {} {} 4 0 3 1 2 ,\"\n" .format(output_name, input_name, output_name)
 
 
 def replace_output_name(layer_info: str, src_name: str, dst_name: str):
@@ -46,7 +44,8 @@ def replace_output_name(layer_info: str, src_name: str, dst_name: str):
 
 
 def fix_tnn_output(tnnproto_path: str):
-    tnnproto = np.loadtxt(tnnproto_path, dtype=np.str, delimiter="\n")
+    with open(tnnproto_path) as f:
+        tnnproto = f.readlines()
     output_info = get_output_info(tnnproto)
     offset = 5
     add_layers = []
@@ -55,21 +54,21 @@ def fix_tnn_output(tnnproto_path: str):
         inner_output_name = output_name + "_fix_output_name_from_tf2onnx"
         add_layers.append(inner_output_name)
         tnnproto[offset + idx] = replace_output_name(tnnproto[offset + idx], output_name, inner_output_name)
-        tnnproto = np.append(tnnproto, generate_transpose(inner_output_name, output_name))
+        tnnproto.append(generate_transpose(inner_output_name, output_name))
 
     # fix op
-    op_name_list = tnnproto[2].split(" ")
-    new_ops_name_list = op_name_list[:1] + add_layers + op_name_list[1:]
-    new_ops_name = " ".join(new_ops_name_list)
-    tnnproto = tnnproto.astype("<U{}" .format(len(new_ops_name) + 1))
-    tnnproto[2] = new_ops_name
-
     info = tnnproto[0].split(" ")
     total_ops = int(info[1]) + len(add_layers)
     info[1] = str(total_ops)
     tnnproto[0] = " ".join(info)
 
+    op_name_list = tnnproto[2].split(" ")
+    new_ops_name_list = op_name_list[:1] + add_layers + op_name_list[1:]
+    new_ops_name = " ".join(new_ops_name_list)
+    tnnproto[2] = new_ops_name
+
     # fix layer cnt
-    layer_cnt = tnnproto[offset:].shape[0]
-    tnnproto[4] = "\" {} ,\"" .format(layer_cnt)
-    np.savetxt(tnnproto_path, tnnproto, delimiter="\n", fmt="%s")
+    layer_cnt = len(tnnproto[offset:])
+    tnnproto[4] = "\" {} ,\"\n" .format(layer_cnt)
+    with open(tnnproto_path, "w") as f:
+        f.writelines(tnnproto)
