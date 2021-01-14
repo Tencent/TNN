28a19eb59d40c81105d8d9009d39261a349916f2
neiltian <neiltian@tencent.com>
2021-01-14 19:02:16 +0800

    Revert "[BUG] fix constant_map incorrect after Reshape"
    This reverts commit 505706e564dd143a523b5bc4fec856c46a347dfb.

--- source/tnn/layer/multidir_broadcast_layer.cc
+++ source/tnn/layer/multidir_broadcast_layer.cc
@@ -155,7 +155,7 @@ Status MultidirBroadcastLayer::InferOutputShape(bool ignore_error) {
         if (!SupportBroadcast(dim0, dim1)) {
             LOGE_IF(!ignore_error, 
                 "Error: operands could not be broadcast together with wrong "
-                "shape (name: %s)\n", layer_param->name.c_str());
+                "shape\n");
             return Status(TNNERR_LAYER_ERR,
                           "Error: operands could not be broadcast together "
                           "with wrong shape");
