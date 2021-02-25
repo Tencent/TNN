948459255cd31c6198ec7d1ad682e31b49a9a322
devandong <devandong@tencent.com>
2021-02-25 20:57:25 +0800

    [BUG] fix insert_layout_reformat bug for constant layers

---
+++ source/tnn/device/metal/acc/metal_reformat_layer_acc.h
@@ -0,0 +1,13 @@
+//
+//  metal_reformat_layer_acc.h
+//  tnn
+//
+//  Created by devandong on 2021/2/24.
+//  Copyright © 2021 tencent. All rights reserved.
+//
+
+#ifndef metal_reformat_layer_acc_h
+#define metal_reformat_layer_acc_h
+
+
+#endif /* metal_reformat_layer_acc_h */
