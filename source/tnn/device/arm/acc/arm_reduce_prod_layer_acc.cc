d743e4d6d0f4c1ff2f95fcf2dfd7f4f3a88bec4e
devandong <devandong@tencent.com>
2021-03-01 14:08:45 +0800

    [BUG] fix issue 816

--- source/tnn/device/arm/acc/arm_reduce_prod_layer_acc.cc
+++ source/tnn/device/arm/acc/arm_reduce_prod_layer_acc.cc
@@ -25,7 +25,7 @@ typedef struct arm_reduce_prod_operator : arm_reduce_operator {
     };
 
     virtual Float4 DataInit() {
-        return Float4(1);
+        return Float4(1.f);
     };
 
     virtual Float4 Calculate(Float4 &v, Float4 &t) {
