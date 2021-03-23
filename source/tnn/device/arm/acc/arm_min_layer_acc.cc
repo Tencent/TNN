929f35b5cb86312eae079f8a1573e33186aa4b79
powerpwang <powerpwang@outlook.com>
2021-03-23 15:56:15 +0800

    [ARM][WIP] update binary op to support multi-dir broadcast

--- source/tnn/device/arm/acc/arm_min_layer_acc.cc
+++ source/tnn/device/arm/acc/arm_min_layer_acc.cc
@@ -26,6 +26,7 @@ Status ArmMinLayerAcc::Init(Context *context, LayerParam *param, LayerResource *
     }
 
     _Operator = [=](Float4 v1, Float4 v2) -> Float4 { return Float4::min(v1, v2); };
+    _OperatorElement = [=](float v1, float v2) -> float { return MIN(v1, v2); };
 
     return TNN_OK;
 }
