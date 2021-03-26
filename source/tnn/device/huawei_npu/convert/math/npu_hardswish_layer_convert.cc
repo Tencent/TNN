5dc78bce5003e5fe54c1648416c0a8366e3896bd
shaundai <shaundai@tencent.com>
2021-03-26 15:38:21 +0800

    [NPU][ADD] add multi input support for hardswish in HUAWEI_NPU

--- source/tnn/device/huawei_npu/convert/math/npu_hardswish_layer_convert.cc
+++ source/tnn/device/huawei_npu/convert/math/npu_hardswish_layer_convert.cc
@@ -13,28 +13,42 @@
 // specific language governing permissions and limitations under the License.
 
 #include <graph/op/all_ops.h>
-#include "npu_unary_operator.h"
 #include "tnn/device/huawei_npu/convert/npu_base_layer_convert.h"
 #include "tnn/device/huawei_npu/convert/npu_utils.h"
 
 namespace TNN_NS {
 
-class NpuHardswishLayer : public NpuUnaryLayer {
-public:
-    NpuHardswishLayer(LayerType ignore) : NpuUnaryLayer(LAYER_HARDSWISH) {}
-    ~NpuHardswishLayer() {}
+DECLARE_NPU_LAYER_WEIGHT(Hardswish, LAYER_HARDSWISH)
 
-protected:
-    Status Convert() {
-        auto param = dynamic_cast<HardSwishLayerParam *>(param_);
-        if (!(param->alpha >= 0.1666f && param->alpha <= 0.1667f && param->beta >= 0.4999f && param->beta <= 0.5001f)) {
-            LOGE("hardswish only support alpha=1/6 beta=0.5, but in fact, alpha=%f beta=%f\n", param->alpha, param->beta);
-            return Status(TNNERR_LAYER_ERR, "Error: Npu currently only supports hardswish (alpha=1/6, beta=0.5)");
-        }
-        return NpuUnaryLayer::UnaryConvert<hiai::op::HardSwish>();
+Status NpuHardswishLayer::Convert() {
+    auto param = dynamic_cast<HardSwishLayerParam *>(param_);
+    if (!(param->alpha >= 0.1666f && param->alpha <= 0.1667f && param->beta >= 0.4999f && param->beta <= 0.5001f)) {
+        LOGE("hardswish only support alpha=1/6 beta=0.5, but in fact, alpha=%f beta=%f\n", param->alpha, param->beta);
+        return Status(TNNERR_LAYER_ERR, "Error: Npu currently only supports hardswish (alpha=1/6, beta=0.5)");
     }
-};
 
-//REGISTER_NPU_LAYER(Hardswish, LAYER_HARDSWISH);
+    int input_size = input_ops_.size();
+    if (input_size == 1) {
+        auto output = std::make_shared<hiai::op::HardSwish>(outputs_name_[0]);
+        output->set_input_x(*input_ops_[0]->GetOperator());
+        ADD_OUTPUT_OP(output)
+    } else if (input_size == 2) {
+        // hardswish will be broken into hardsigmoid+mul
+        auto sub_op = std::make_shared<ge::op::Activation>(layer_name_ + "_sigmoid");
+        sub_op->set_input_x(*input_ops_[1]->GetOperator());
+        sub_op->set_attr_mode(10);
+        weight_ops_.push_back(sub_op);
+
+        auto output = std::make_shared<hiai::op::Mul>(outputs_name_[0]);
+        output->set_input_x1(*input_ops_[0]->GetOperator());
+        output->set_input_x2(*sub_op);
+        ADD_OUTPUT_OP(output)
+    } else {
+        printf("the Unary input size is not correct\n");
+        return Status(TNNERR_PARAM_ERR, "Error: the Unary layer count is not correct");
+    }
+}
+
+REGISTER_NPU_LAYER(Hardswish, LAYER_HARDSWISH);
 
 }  // namespace TNN_NS
