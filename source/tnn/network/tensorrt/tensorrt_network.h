801fb5ed4029f191627c59160a3ba6661748c5fc
johnzlli-tencent <1016963088@qq.com>
2021-03-26 14:53:35 +0800

    [CUDA][UPD][FIX]update tensorrt network;fix cuda einsum

--- source/tnn/network/tensorrt/tensorrt_network.h
+++ source/tnn/network/tensorrt/tensorrt_network.h
@@ -89,7 +89,7 @@ public:
 
     static std::unordered_map<std::string, TensorRTPluginLayerBuilder*> GetPluginLayerNameMap();
 
-    std::string GetCacheFileName(std::string cfg, std::string model, BlobMap input_map,
+    std::string GetCacheFileName(std::vector<std::string> params_md5, BlobMap input_map,
         BlobMap output_map, const InputShapesMap &min_inputs_shape, int device_id, int batchsize,
         bool int8_mode, bool use_fp16);
 
