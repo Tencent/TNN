bd934e41fefcf8d04f5eb84654a7226d64b8252c
PeterWuKK <peng.wu@intel.com>
2021-03-31 09:45:45 -0400

    [OPENVINO] fix licenses: from Apache -> BSD-3

--- source/tnn/network/openvino/layer_builder/compute/dnnl_common.h
+++ source/tnn/network/openvino/layer_builder/compute/dnnl_common.h
@@ -1,11 +1,10 @@
 /*******************************************************************************
-* Copyright 2018-2020 Intel Corporation
+* Copyright 2020 Intel Corporation
 *
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
+* Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at
+* 
+* https://opensource.org/licenses/BSD-3-Clause0
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
