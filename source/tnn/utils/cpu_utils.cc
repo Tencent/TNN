7cacaa725321f518a3bd117d32468209a4f6538a
powerpwang <powerpwang@outlook.com>
2021-06-03 15:49:19 +0800

    [ARM] add detect int8 dot product for arm linux

--- source/tnn/utils/cpu_utils.cc
+++ source/tnn/utils/cpu_utils.cc
@@ -523,9 +523,42 @@ bool CpuUtils::CpuSupportInt8Dot() {
     return false;
 #endif
 
+// Linux
+#elif defined(__linux__)
+    cpuinfo_android_properties cpu_prop;
+    cpuinfo_arm_linux_processor processor;
+    cpuinfo_arm_linux_parse_proc_cpuinfo(cpu_prop.proc_cpuinfo_hardware, &processor);
+
+    // we use table lookup to check dot product support
+    switch (processor.midr & (CPUINFO_ARM_MIDR_IMPLEMENTER_MASK | CPUINFO_ARM_MIDR_PART_MASK)) {
+        case UINT32_C(0x4100D060): /* Cortex-A65 */
+        case UINT32_C(0x4100D0B0): /* Cortex-A76 */
+        case UINT32_C(0x4100D0C0): /* Neoverse N1 */
+        case UINT32_C(0x4100D0D0): /* Cortex-A77 */
+        case UINT32_C(0x4100D0E0): /* Cortex-A76AE */
+        case UINT32_C(0x4100D4A0): /* Neoverse E1 */
+        case UINT32_C(0x4800D400): /* Cortex-A76 (HiSilicon) */
+        case UINT32_C(0x51008040): /* Kryo 485 Gold (Cortex-A76) */
+        case UINT32_C(0x51008050): /* Kryo 485 Silver (Cortex-A55) */
+        case UINT32_C(0x53000030): /* Exynos M4 */
+        case UINT32_C(0x53000040): /* Exynos M5 */
+        case UINT32_C(0x4800d010): /* Kunpeng 920 */
+            int8dot = true;
+            break;
+        case UINT32_C(0x4100D050): /* Cortex A55: revision 1 or later only */
+            int8dot = !!(((processor.midr & UINT32_C(0x00F00000)) >> 20) >= 1);
+            break;
+        case UINT32_C(0x4100D0A0): /* Cortex A75: revision 2 or later only */
+            int8dot = !!(((processor.midr & UINT32_C(0x00F00000)) >> 20) >= 2);
+            break;
+    }
+
+    LOGD("CpuUtils::CpuSupportInt8Dot, ANDROID, midr = %x, int8dot = %d.\n", processor.midr, int8dot);
+    return int8dot;
+
 // unknown
 #else
-    LOGE("CpuUtils::CpuSupportInt8Dot, unknown platform, int8dot = 0.\n");
+    LOGD("CpuUtils::CpuSupportInt8Dot, unknown platform, int8dot = 0.\n");
     return false;
 #endif
 
