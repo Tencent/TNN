c2793f7722504b751dbee967603509c867122138
johnzlli-tencent <1016963088@qq.com>
2021-06-21 16:48:12 +0800

    [CUDA]add shape tensor for cuda and refactor stride_slice_v2

--- source/tnn/network/tensorrt/layer_builder/strided_slice_v2_layer_builder.cc
+++ source/tnn/network/tensorrt/layer_builder/strided_slice_v2_layer_builder.cc
@@ -12,68 +12,112 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-#include "tnn/network/tensorrt/layer_builder/tensorrt_plugin_layer_builder.h"
+#include <cassert>
+#include <stdlib.h>
+#include <unordered_set>
+
+#include "tnn/network/tensorrt/layer_builder/tensorrt_layer_builder.h"
 
 namespace TNN_NS {
 
-DECLARE_TENSORRT_PLUGIN_LAYER_BUILDER(StrideSliceV2, LAYER_STRIDED_SLICE_V2);
+DECLARE_TENSORRT_LAYER_BUILDER(StrideSliceV2, LAYER_STRIDED_SLICE_V2);
 
-bool StrideSliceV2TRTPluginLayerBuilder::supportsFormatCombination(
-        int pos, const nvinfer1::PluginTensorDesc* inOut, int nbInputs, int nbOutputs) {
-    return inOut[pos].type == nvinfer1::DataType::kFLOAT || inOut[pos].type == nvinfer1::DataType::kINT32;
+static ShapeTensor clamp(INetworkDefinition* network, const ShapeTensor& x, const ShapeTensor& lowerBound,
+        const ShapeTensor& upperBound) {
+    return min(network, max(network, x, lowerBound), upperBound);
 }
 
-Status StrideSliceV2TRTPluginLayerBuilder::Reshape() {
-    return m_layer->Reshape();
+static ShapeTensor bumpIfNegative(INetworkDefinition* network, const ShapeTensor& inputDims,
+        const ShapeTensor& indices) {
+    const auto signs = clamp(network, indices, shapeVector(-1), shapeVector(0));
+    return sub(network, indices, mul(network, signs, inputDims));
 }
 
-const char* StrideSliceV2TRTPluginLayerBuilder::getPluginType() const {
-    return "StrideSliceV2";
+void decodeOnnxStartsAndEnds(INetworkDefinition* network, const ShapeTensor& inputDims,
+        const ShapeTensor& steps, ShapeTensor& starts, ShapeTensor& ends) {
+    const auto stepSign = clamp(network, steps, shapeVector(-1), shapeVector(0));
+    starts = bumpIfNegative(network, inputDims, starts);
+    starts = clamp(network, starts, shapeVector(0), add(network, inputDims, stepSign));
+    ends = bumpIfNegative(network, inputDims, ends);
+    ends = clamp(network, ends, stepSign, inputDims);
 }
 
-nvinfer1::DataType StrideSliceV2TRTPluginLayerBuilder::getOutputDataType(int index, const nvinfer1::DataType* inputTypes,
-        int nbInputs) const {
-    return inputTypes[0];
-}
-
-ILayer* StrideSliceV2TRTPluginLayerBuilder::AddToNetwork(INetworkDefinition* network) {
-    return TensorRTPluginLayerBuilder::AddToNetwork(network);
-}
-
-DimsExprs StrideSliceV2TRTPluginLayerBuilder::getOutputDimensions(int index, const nvinfer1::DimsExprs* inputs,
-        int nbInputs, nvinfer1::IExprBuilder& exprBuilder) {
-    StrideSliceV2LayerParam* param = dynamic_cast<StrideSliceV2LayerParam*>(param_);
-    DimsExprs output(inputs[0]);
-    for (int i = 0; i < param->axes.size(); i++) {
-        int index = param->axes[i];
-
-        auto begin = exprBuilder.constant(param->begins[i]);
-        if (param->begins[i] < 0) {
-            begin = exprBuilder.operation(DimensionOperation::kSUM, *begin, *inputs[0].d[index]);
-        }
-
-        auto end = exprBuilder.constant(param->ends[i]);
-        if (param->ends[i] == INT_MAX) {
-            end = inputs[0].d[index];
-        }
-
-        if (param->ends[i] < 0) {
-            end = exprBuilder.operation(DimensionOperation::kSUM, *end, *inputs[0].d[index]);
-        }
-        auto stride = exprBuilder.constant(param->strides[i]);
-        auto one = exprBuilder.constant(1);
-        auto diff = exprBuilder.operation(DimensionOperation::kSUB, *end, *begin);
-        diff = exprBuilder.operation(DimensionOperation::kSUB, *diff, *one);
-        output.d[index] = exprBuilder.operation(DimensionOperation::kFLOOR_DIV, *diff, *stride);
-        output.d[index] = exprBuilder.operation(DimensionOperation::kSUM, *output.d[index], *one);
+ShapeTensor axesToInterlaceSubscripts(const ShapeTensor& axes, int nbDims) {
+    std::vector<int> subscripts(nbDims);
+    std::iota(subscripts.begin(), subscripts.end(), 0);
+    for (int i = 0; i < axes.size(); ++i) {
+        subscripts[axes[i]] = nbDims + i;
     }
-    return output;
+    return ShapeTensor(1, std::move(subscripts));
 }
 
-const char* StrideSliceV2PluginCreator::getPluginName() const {
-    return "StrideSliceV2";
+ShapeTensor computeSliceSizes(INetworkDefinition* network, const ShapeTensor& starts, const ShapeTensor& ends,
+        const ShapeTensor& steps, const ShapeTensor& dims) {
+    if (steps.isAll(1)) {
+        return sub(network, ends, starts);
+    } else {
+        return sub(network, similar(network, dims, 0), floorDiv(network, sub(network, starts, ends), steps));
+    }
 }
 
-REGISTER_TENSORRT_PLUGIN_LAYER_BUILDER(StrideSliceV2, LAYER_STRIDED_SLICE_V2);
+ILayer* StrideSliceV2TRTLayerBuilder::AddToNetwork(INetworkDefinition* network) {
+    StrideSliceV2LayerParam* param = dynamic_cast<StrideSliceV2LayerParam*>(param_);
+    auto input_tensors = GetInputITensors();
+    auto dims = shapeOf(*input_tensors[0]);
+
+    ShapeTensor begins;
+    ShapeTensor ends;
+    ShapeTensor axes;
+    ShapeTensor strides;
+
+    begins = ShapeTensor(1, std::move(param->begins));
+    ends = ShapeTensor(1, std::move(param->ends));
+    axes = ShapeTensor(1, std::move(param->axes));
+    strides = ShapeTensor(1, std::move(param->strides));
+
+    if (input_tensors.size() >= 2) {
+        begins = ShapeTensor(*input_tensors[1], 0);
+    }
+
+    if (input_tensors.size() >= 3) {
+        ends = ShapeTensor(*input_tensors[2], 0);
+    }
+
+    bool isIota = true;
+    int j = 0;
+    std::vector<int> newAxes;
+    newAxes.reserve(axes.size());
+
+    for (int axis : axes) {
+        int r = dims.size();
+        assert(-r <= axis && axis < r);
+        if (axis < 0) {
+            axis += r;
+        }
+        newAxes.push_back(axis);
+        isIota &= axis == j;
+        ++j;
+    }
+    axes = ShapeTensor(1, std::move(newAxes));
+
+    assert(std::unordered_set<int>(axes.begin(), axes.end()).size() == static_cast<size_t>(axes.size()));
+
+    if (axes.size() < dims.size() || !isIota) {
+        const ShapeTensor subscripts{axesToInterlaceSubscripts(axes, dims.size())};
+        begins = interlace(network, similar(network, dims, 0), begins, subscripts);
+        ends = interlace(network, dims, ends, subscripts);
+        strides = interlace(network, similar(network, dims, 1), strides, subscripts);
+    }
+
+    decodeOnnxStartsAndEnds(network, dims, strides, begins, ends);
+    const ShapeTensor sizes = computeSliceSizes(network, begins, ends, strides, dims);
+    nvinfer1::ISliceLayer* layer = addSlice(network, *input_tensors[0], begins, sizes, strides);
+    if (layer != nullptr) {
+        layer->setName(layer_name_.c_str());
+    }
+    return layer;
+}
+
+REGISTER_TENSORRT_LAYER_BUILDER(StrideSliceV2, LAYER_STRIDED_SLICE_V2);
 
 }  //  namespace TNN_NS
