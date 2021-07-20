006d5b89613a0e07f0baa49643e99c92e077b31f
neiltian-tencent <neiltian@tencent.com>
2021-07-20 06:13:40 +0000

    [pytnn] support more input shapes type

--- source/pytnn/module.py
+++ source/pytnn/module.py
@@ -1,10 +1,49 @@
+from typing import List, Dict, Any
 import pytnn
 
-def Load(model_path, network_config=None, min_input_shapes={}, max_input_shapes={}):
+def _supported_input_size_type(input_size) -> bool:
+    if isinstance(input_size, tuple):
+        return True
+    elif isinstance(input_size, list):
+        return True
+    else:
+        raise TypeError(
+            "Input sizes for inputs are required to be a List, tuple or a Dict of two sizes (min, max), found type: "
+            + str(type(input_size)))
+
+
+def _parse_input_ranges(input_sizes: List):
+    print(input_sizes)
+    if any(not isinstance(i, dict) and not _supported_input_size_type(i) for i in input_sizes):
+        raise KeyError("An input size must either be a static size or a range of two sizes (min, max) as Dict")
+    min_input_shapes = {}
+    max_input_shapes = {}
+    for index, value in enumerate(input_sizes):
+        if isinstance(value, dict):
+            if all(k in value for k in ["min", "max"]):
+                min_input_shapes["input_" + str(index)] = value["min"];
+                max_input_shapes["input_" + str(index)] = value["max"];
+            else:
+                raise KeyError(
+                    "An input size must either be a static size or a range of three sizes (min, opt, max) as Dict")
+        elif isinstance(value, list):
+            min_input_shapes["input_" + str(index)] = value;
+            max_input_shapes["input_" + str(index)] = value;
+        elif isinstance(value, tuple):
+            min_input_shapes["input_" + str(index)] = value;
+            max_input_shapes["input_" + str(index)] = value;
+    return (min_input_shapes, max_input_shapes)
+
+
+def load(model_path, network_config=None, min_input_shapes={}, max_input_shapes={}):
     return Module(model_path, network_config, min_input_shapes, max_input_shapes)
 
-def Load(model_path, network_config=None, input_shapes={}):
+def load(model_path, network_config=None, input_shapes={}):
     return Module(model_path, network_config, input_shapes, input_shapes)
+
+def load(model_path, config_dict):
+    min_input_shapes, max_input_shapes = _parse_input_ranges(config_dict["input_shapes"])
+    return Module(model_path, None, min_input_shapes, max_input_shapes)
 
 class Module:
     def __init__(self, model_path, network_config, min_input_shapes, max_input_shapes):
--- test/pytnn/test_simple_api.py
+++ test/pytnn/test_simple_api.py
@@ -1,7 +1,11 @@
 import module
 import numpy as np
 
-module=module.Load("../../model/SqueezeNet/squeezenet_v1.1.ts", input_shapes={"input_0": [1,3,224,224]})
 input=np.ones((1,3,224,224), np.float32, 'F')
+
+#module=module.load("../../model/SqueezeNet/squeezenet_v1.1.ts", input_shapes={"input_0": [1,3,224,224]})
+#module=module.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[[1,3,224,224]]})
+#module=module.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[(1,3,224,224)]})
+module=module.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[ {"min": [1,3,224,224], "max": [1,3,224,224]} ]})
 output=module.forward(input)
 print(output)
