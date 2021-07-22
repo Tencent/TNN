f5e32f50a8fd5da59d9efe7f71ace53219bafd1c
neiltian-tencent <neiltian@tencent.com>
2021-07-22 12:26:23 +0000

    [pytnn] support for more input string type

--- test/pytnn/test_simple_api.py
+++ test/pytnn/test_simple_api.py
@@ -5,14 +5,16 @@ input=np.ones((1,3,224,224), np.float32, 'F')
 
 # torchscript 
 #module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[ {"min": [1,3,224,224], "max": [1,3,224,224]} ], "network_type": pytnn.NETWORK_TYPE_TNNTORCH, "device_type": pytnn.DEVICE_CUDA})
+module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[ {"min": [1,3,224,224], "max": [1,3,224,224]} ], "network_type": "tnntorch", "device_type": "cuda"})
 #module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[ {"min": [1,3,224,224], "max": [1,3,224,224]} ]})
 #module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[[1,3,224,224]]})
-module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[(1,3,224,224)]})
+#module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.ts", {"input_shapes":[(1,3,224,224)]})
 output=module.forward(input)
 print(output[0])
 
 # tnnproto
-module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.tnnproto")
+module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.tnnproto", {"device_type": "naive", "precision": "FP16"})
+#module=pytnn.load("../../model/SqueezeNet/squeezenet_v1.1.tnnproto")
 #output=module.forward({"data": input}, rtype="dict")
 #for key, value in output.items():
 #    print (key, value)
