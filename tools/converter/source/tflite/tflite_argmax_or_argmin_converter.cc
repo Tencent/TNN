8b80b23bb0724a3eb6b61485846420a214d3dff1
ealinli <37806708+1627180283@users.noreply.github.com>
2021-08-10 20:44:24 -0700

    Fix typo

--- tools/converter/source/tflite/tflite_argmax_or_argmin_converter.cc
+++ tools/converter/source/tflite/tflite_argmax_or_argmin_converter.cc
@@ -40,7 +40,7 @@ TNN_NS::Status TFLiteArgMaxOrMinConverter::exec(
     param->name      = cur_layer->name;
     param->type      = cur_layer->type_str;
     param->quantized = false;
-    // tflite alway keep_dims = 0
+    // tflite always keep_dims = 0
     param->keep_dims    = 0;
     auto tflite_op_type = tf_lite_op_set[tf_lite_operator->opcode_index]->builtin_code;
     if (tflite_op_type == tflite::BuiltinOperator_ARG_MIN) {
