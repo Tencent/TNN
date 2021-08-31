0bc50f3d993d4a5931d983190ee9edde64a4740b
shenpenwang <41420892+Maosquerade@users.noreply.github.com>
2021-08-31 17:16:44 +0800

    Feature demo stream (#1028)
    * [fix] add git ignore on macos
    
    * [demo] add stream input for face detect demo on x86
    
    * [demo] add obeject detector with camera on x86
    
    * [demo] cuse yolo object to detect
    
    * [demo] add face align demo with face_mesh_model
    
    * [demo] change face align demo with youtu_face_aligb model
    
    * [demo] merge from master; fix openvino strided slice error with strides
    
    * [demo] fix pose detect demo
    
    * [demo] add skeleton test image
    
    * [demo] add hair segmentation demo for x86
    
    * [DOC] update compilation for windows cuda
    
    * [DEMO][YOLOv5] adapt to feature_dynamic dims yolov5
    
    * [demo] fix building stage for windows; fix some source file builing error on windows
    
    * [model] add download_model.bat for windows
    
    * [demo] add ignore rules
    
    * [demo] set linux cuda CMakeLists
    
    * [demo] add ocr text demo for x86
    
    * [demo] add ocr detect demo
    
    * [demo] fix changes for cuda ocr detector
    
    * [demo] support demo on windows cuda; update documents
    
    * [BUILD] add cudnn_adv_infer lib link on windows cuda
    
    * [openvino] fix custom openvino reshape support
    
    * [fix] openvino network build with enable_const_folder
    
    * [fix] fix pool and lstmonnx layer reshape order
    
    * [CUDA] fix lstm reshape logic
    
    * [OPENVINO] update openvino version on macos
    
    * [demo] remove computeUnits openvino/tensorrt, choose network by default
    
    * [DEMO] update documents for demo with ComputeUnitsOpenvino/ComputeUnitsTensorRT removed
    
    * [FIX] remove print and redundant defination
    
    * [DEMO] update yolov5 model; update demo document
    
    * [DEMO] update yolov5 model path in documents
    
    * [demo] add demo support on readme documents
    
    * [DOC] add demo sdk document attention that sdk should bind specific models
    
    * [SCR] add gitignore on build_linux_native
    
    * [OPENVINO] updata custom implementation shape inference;
    update upsample layer default mode to half_pixel;
    update deconv layer build when output_padding set;
    update reshape layer builder rules for custom reshape
    
    * [OPENVINO] update deconv builder rules
    
    Co-authored-by: Maosquerade <maosquerade@gmail.com>
    Co-authored-by: seanxcwang <seanxcwang@tencent.com>
    Co-authored-by: lucasktian <lucasktian@tencent.com>

--- examples/linux/src/TNNHairSegmentation/TNNHairSegmentation.cc
+++ examples/linux/src/TNNHairSegmentation/TNNHairSegmentation.cc
@@ -16,7 +16,7 @@
 #include <string>
 #include <vector>
 
-#include "ultra_face_detector.h"
+#include "hair_segmentation.h"
 #include "tnn_sdk_sample.h"
 #include "macro.h"
 #include "utils/utils.h"
@@ -35,7 +35,6 @@
 #endif
 using namespace TNN_NS;
 
-// model: face
 int main(int argc, char** argv) {
     if (!ParseAndCheckCommandLine(argc, argv, false)) {
         ShowUsage(argv[0]);
@@ -75,12 +74,12 @@ int main(int argc, char** argv) {
         std::cin.getline(img_buff, 256);
         std::cin.getline(img_buff, 256);
         if (strlen(img_buff) == 0) {
-            strncpy(input_imgfn, "../../../assets/test_face.jpg", 256);
+            strncpy(input_imgfn, "../../../assets/test_blazeface.jpg", 256);
         } else {
             strncpy(input_imgfn, img_buff, 256);
         }
-        printf("Face-detector is about to start, and the picrture is %s\n", input_imgfn);
-        data = stbi_load(input_imgfn, &image_width, &image_height, &image_channel, 3);
+        printf("Hair-segmentation is about to start, and the picrture is %s\n", input_imgfn);
+        data = stbi_load(input_imgfn, &image_width, &image_height, &image_channel, 4);
         if (!data) {
             std::cerr << "Image open failed.\n";
             return -1;
@@ -112,7 +111,7 @@ int main(int argc, char** argv) {
     auto model_content = fdLoadFile(FLAGS_m.c_str());
    // int h = 240, w = 320;
 
-    auto option = std::make_shared<UltraFaceDetectorOption>();
+    auto option = std::make_shared<HairSegmentationOption>();
     {
         option->proto_content = proto_content;
         option->model_content = model_content;
@@ -122,12 +121,9 @@ int main(int argc, char** argv) {
         #ifdef _CUDA_
             option->compute_units = TNN_NS::TNNComputeUnitsGPU;
         #endif
-    
-        option->score_threshold = 0.95;
-        option->iou_threshold = 0.15;
     }
     
-    auto predictor = std::make_shared<UltraFaceDetector>();
+    auto predictor = std::make_shared<HairSegmentation>();
 
     // Init
     std::shared_ptr<TNNSDKOutput> sdk_output = predictor->CreateSDKOutput();
@@ -140,6 +136,7 @@ int main(int argc, char** argv) {
             cap >> frame;
             if (frame.empty()) break;
             MatType mat_type = N8UC3;
+            cv::cvtColor(frame, frame, cv::COLOR_RGB2RGBA);
             image_width = frame.cols;
             image_height = frame.rows;
             image_channel = frame.channels();
@@ -147,57 +144,37 @@ int main(int argc, char** argv) {
             data = frame.ptr();
         }
 #endif
-        DimsVector orig_dims = {1, 3, image_height, image_width};
-
+        DimsVector orig_dims = {1, 4, image_height, image_width};
         //Predict
-        auto image_mat = std::make_shared<TNN_NS::Mat>(TNN_NS::DEVICE_NAIVE, TNN_NS::N8UC3, orig_dims, data);
-        auto resize_mat = predictor->ProcessSDKInputMat(image_mat, "data_input");
-        CHECK_TNN_STATUS(predictor->Predict(std::make_shared<UltraFaceDetectorInput>(resize_mat), sdk_output));
-        std::vector<FaceInfo> face_info;
-        if (sdk_output && dynamic_cast<UltraFaceDetectorOutput *>(sdk_output.get())) {
-            auto face_output = dynamic_cast<UltraFaceDetectorOutput *>(sdk_output.get());
-            face_info = face_output->face_list;
+
+        auto image_mat = std::make_shared<TNN_NS::Mat>(TNN_NS::DEVICE_NAIVE, TNN_NS::N8UC4, orig_dims, data);
+        CHECK_TNN_STATUS(predictor->Predict(std::make_shared<TNNSDKInput>(image_mat), sdk_output));
+        // CHECK_TNN_STATUS(predictor->ProcessSDKOutput(sdk_output));
+        ImageInfo merged_image;
+        if (sdk_output && dynamic_cast<HairSegmentationOutput *>(sdk_output.get())) {
+            auto hair_output = dynamic_cast<HairSegmentationOutput *>(sdk_output.get());
+            merged_image = hair_output->merged_image;
         }
 
-        const int image_orig_height = int(image_height);
-        const int image_orig_width  = int(image_width);
-        const auto& target_dims     = predictor->GetInputShape();
-        const int target_height     = target_dims[2];
-        const int target_width      = target_dims[3];
-        float scale_x               = image_orig_width / (float)target_width;
-        float scale_y               = image_orig_height / (float)target_height;
-
-        //convert rgb to rgb-a
-        uint8_t *ifm_buf = new uint8_t[image_width*image_height*4];
-        for (int i = 0; i < image_width * image_height; ++i) {
-            ifm_buf[i*4]   = data[i*3];
-            ifm_buf[i*4+1] = data[i*3+1];
-            ifm_buf[i*4+2] = data[i*3+2];
-            ifm_buf[i*4+3] = 255;
-        }
-        for (int i = 0; i < face_info.size(); i++) {
-            auto face = face_info[i];
-            TNN_NS::Rectangle((void *)ifm_buf, image_orig_height, image_orig_width, face.x1, face.y1, face.x2,
-                    face.y2, scale_x, scale_y);
-        }
+        auto ifm_buf = (uint8_t*)(merged_image.data.get());
 
 #ifdef _OPENCV_
         if (detect_type != 1) {
-            cv::Mat face_frame(image_height, image_width, CV_8UC4, ifm_buf);
-            cv::imshow("face_dectecting" ,face_frame);
+            cv::Mat hair_frame(image_height, image_width, CV_8UC4, ifm_buf);
+            cv::imshow("hair_segmentation" ,hair_frame);
 
-            delete [] ifm_buf;
+            // delete [] ifm_buf;
             auto key_num = cv::waitKey(30);
             if (key_num == 'q') break;
         } else {
 #endif
             char buff[256];
             sprintf(buff, "%s.png", "predictions");
-            int success = stbi_write_bmp(buff, image_orig_width, image_orig_height, 4, ifm_buf);
+            int success = stbi_write_bmp(buff, merged_image.image_width, merged_image.image_height, merged_image.image_channel, ifm_buf);
             if(!success) 
                 return -1;
-            delete [] ifm_buf;
-            fprintf(stdout, "Face-detector done.\nNumber of faces: %d\n",int(face_info.size()));
+            printf("Hair Segmentation Done. The result was saved in %s.png\n", "predictions");
+            // delete [] ifm_buf;
             free(data);
 #ifdef _OPENCV_
             break;
