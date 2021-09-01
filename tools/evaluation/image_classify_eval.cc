48f49d85a13b6e04ec6c5898af2440278f0c0ef9
realaeon <liwwchn@gmail.com>
2021-09-01 10:37:00 +0800

    batch_size and openmp support for evaluation

--- tools/evaluation/image_classify_eval.cc
+++ tools/evaluation/image_classify_eval.cc
@@ -4,13 +4,15 @@
 
 #include <cstring>
 
-#include "image_classifier.h"
+#include "image_classifier_sdk_eval.h"
 #include "macro.h"
 #include "stb_image.h"
 #include "utils.h"
-#include "tnn/utils/omp_utils.h"
 // #include "stb_image_resize.h"
 // #include "stb_image_write.h"
+#include "tnn/utils/mat_converter_utils.h"
+#include "tnn/utils/omp_utils.h"
+#include "tnn_sdk_sample.h"
 
 using namespace TNN_NS;
 
@@ -30,7 +32,6 @@ int main(int argc, char** argv) {
     std::string model_file_name;
     std::string input_path;
     std::string ground_truth;
-    omp_set_num_threads(8);
 
     struct option long_options[] = {{"proto", required_argument, 0, 'p'},
                                     {"model", required_argument, 0, 'm'},
@@ -92,9 +93,8 @@ int main(int argc, char** argv) {
 #endif
     }
 
-
     // Init predictor
-    auto predictor = std::make_shared<ImageClassifier>();
+    auto predictor = std::make_shared<ImageClassifierEval>();
     std::shared_ptr<TNNSDKOutput> sdk_output = predictor->CreateSDKOutput();
     CHECK_TNN_STATUS(predictor->Init(option));
 
@@ -124,32 +124,49 @@ int main(int argc, char** argv) {
     printf("total images:%d\n", count);
     std::sort(files.begin(), files.end());
 
-    int tested_num = 0;
-    int top1     = 0;
+    int tested_num   = 0;
+    int top1         = 0;
+    int batch_size   = predictor->GetInputShape()[0];
+    auto target_dims = {batch_size, 3, 224, 224};
+    auto image_dims  = {1, 3, 224, 224};
+
+    // set openmp NumThreads 
+    // CHECK_TNN_STATUS(predictor->SetOMPThreads(8));
+    CHECK_TNN_STATUS(predictor->SetOMPThreads(batch_size));
 
     // Predict images
-    for (int i=0; i< count; i++) {
-        const auto& file = files[i];
-        const auto image = input_path + "/" + file;
+    for (int count_idx = 0; count_idx < count / batch_size; count_idx++) {
+        std::shared_ptr<Mat> mat_new(new Mat(TNN_NS::DEVICE_NAIVE, TNN_NS::N8UC3, target_dims));
+        for (int batch_idx = 0; batch_idx < batch_size; batch_idx++) {
+            const auto image = input_path + "/" + files[count_idx * batch_size + batch_idx];
+            int image_width, image_height, image_channel;
+            unsigned char* data   = stbi_load(image.c_str(), &image_width, &image_height, &image_channel, 3);
+            std::vector<int> nchw = {1, image_channel, image_height, image_width};
+            auto input_mat        = std::make_shared<TNN_NS::Mat>(TNN_NS::DEVICE_NAIVE, TNN_NS::N8UC3, nchw, data);
+            auto target_mat =
+                std::make_shared<TNN_NS::Mat>(input_mat->GetDeviceType(), input_mat->GetMatType(), image_dims);
+            CHECK_TNN_STATUS(predictor->Resize(input_mat, target_mat, TNNInterpLinear));
+            int bytesize_perbatch = DimsVectorUtils::Count(target_dims, 1) * GetMatElementSize(target_mat.get());
+            memcpy((char*)mat_new->GetData() + batch_idx * bytesize_perbatch, target_mat->GetData(), bytesize_perbatch);
+            free(data);
+        }
+        predictor->Predict(std::make_shared<TNNSDKInput>(mat_new), sdk_output);
 
-        int image_width, image_height, image_channel;
-        unsigned char* data   = stbi_load(image.c_str(), &image_width, &image_height, &image_channel, 3);
-        std::vector<int> nchw = {1, image_channel, image_height, image_width};
-        auto image_mat        = std::make_shared<TNN_NS::Mat>(TNN_NS::DEVICE_NAIVE, TNN_NS::N8UC3, nchw, data);
-        predictor->Predict(std::make_shared<TNNSDKInput>(image_mat), sdk_output);
-
-        int class_id = -1;
-        if (sdk_output && dynamic_cast<ImageClassifierOutput*>(sdk_output.get())) {
-            auto classfy_output = dynamic_cast<ImageClassifierOutput*>(sdk_output.get());
+        int* class_id;
+        if (sdk_output && dynamic_cast<ImageClassifierEvalOutput*>(sdk_output.get())) {
+            auto classfy_output = dynamic_cast<ImageClassifierEvalOutput*>(sdk_output.get());
             class_id            = classfy_output->class_id;
         }
-        if (class_id == groundTruthId[i]) {
-            top1++;
+        for (int batch_idx = 0; batch_idx < batch_size; batch_idx++) {
+            if (class_id[batch_idx] == groundTruthId[count_idx * batch_size + batch_idx]) {
+                top1++;
+            }
         }
-        if (++tested_num % 10 == 0) {
+        tested_num += batch_size;
+        if (count_idx % 2 == 0) {
             printf("==> tested:%d/%d, Top1: %f\n", tested_num, count, (float)top1 / (float)tested_num * 100.0);
         }
-        free(data);
+        free(class_id);
     }
     printf("==> Done! Final Top1: %f\n", (float)top1 / (float)tested_num * 100.0);
     return 0;
