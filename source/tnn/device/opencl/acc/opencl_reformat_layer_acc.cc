6e580e73dffdba3652b5bbd41a0f78430aa8b8a4
ealinli <ealinli@tencent.com>
2021-09-02 11:48:41 +0800

    [UPD] 1. add new pattern for LSTM(Keras) 2. opencl reformat support multi input

--- source/tnn/device/opencl/acc/opencl_reformat_layer_acc.cc
+++ source/tnn/device/opencl/acc/opencl_reformat_layer_acc.cc
@@ -23,6 +23,8 @@ Status OpenCLReformatLayerAcc::Init(Context *context, LayerParam *param, LayerRe
     Status ret = OpenCLLayerAcc::Init(context, param, resource, inputs, outputs);
     CHECK_TNN_OK(ret)
 
+    ASSERT(inputs.size() == outputs.size());
+
     run_3d_ndrange_ = false;
     op_name_        = "Reformat";
 
@@ -41,7 +43,12 @@ Status OpenCLReformatLayerAcc::Init(Context *context, LayerParam *param, LayerRe
     }
 
     // create kernel
-    ret                     = CreateExecuteUnit(execute_units_[0], "image_to_image", kernel_name_);
+    const int blob_size = inputs.size();
+    execute_units_.resize(blob_size);
+    for (int i = 0; i < blob_size; i++) {
+        ret = CreateExecuteUnit(execute_units_[i], "image_to_image", kernel_name_);
+    }
+
     if (ret != TNN_OK) {
         LOGE("create execute unit failed!\n");
         return ret;
@@ -54,37 +61,38 @@ OpenCLReformatLayerAcc::~OpenCLReformatLayerAcc() {}
 
 Status OpenCLReformatLayerAcc::Reshape(const std::vector<Blob *> &inputs, const std::vector<Blob *> &outputs) {
     LOGD("Reformat Layer Reshape\n");
-    ASSERT(inputs.size() == 1);
     Status ret = OpenCLLayerAcc::Reshape(inputs, outputs);
     CHECK_TNN_OK(ret)
 
+    const int blob_size = inputs.size();
+    for (int i = 0; i < blob_size; i++) {
+        auto input_dims  = inputs[i]->GetBlobDesc().dims;
+        auto output_dims = outputs[i]->GetBlobDesc().dims;
 
-    auto input_dims = inputs[0]->GetBlobDesc().dims;
-    auto output_dims = outputs[0]->GetBlobDesc().dims;
+        if (input_dims != output_dims || input_dims.size() != 3) {
+            LOGE("Reformat Layer input dims invalid\n");
+            return Status(TNNERR_OPENCL_ACC_RESHAPE_ERROR, "Reformat Layer input dims invalid");
+        }
 
-    if (input_dims != output_dims || input_dims.size() != 3) {
-        LOGE("Reformat Layer input dims invalid\n");
-        return Status(TNNERR_OPENCL_ACC_RESHAPE_ERROR, "Reformat Layer input dims invalid");
+        int n = DimsFunctionUtils::GetDim(input_dims, 0);
+        int c = DimsFunctionUtils::GetDim(input_dims, 1);
+        int h = DimsFunctionUtils::GetDim(input_dims, 2);
+        if (kernel_name_ == "NHC4W4ImageToCNH4Image") {
+            execute_units_[i].global_work_size = {static_cast<uint32_t>(UP_DIV(h, 4)), static_cast<uint32_t>(c * n)};
+            execute_units_[i].local_work_size  = {64, 1};
+        } else if (kernel_name_ == "CNH4ImageToNHC4W4Image") {
+            execute_units_[i].global_work_size = {static_cast<uint32_t>(UP_DIV(c, 4)), static_cast<uint32_t>(n * h)};
+            execute_units_[i].local_work_size  = {1, 64};
+        }
+
+        uint32_t idx = 0;
+        execute_units_[i].ocl_kernel.setArg(idx++, execute_units_[i].global_work_size[0]);
+        execute_units_[i].ocl_kernel.setArg(idx++, execute_units_[i].global_work_size[1]);
+        execute_units_[i].ocl_kernel.setArg(idx++, *((cl::Image *)inputs[i]->GetHandle().base));
+        execute_units_[i].ocl_kernel.setArg(idx++, n);
+        execute_units_[i].ocl_kernel.setArg(idx++, h);
+        execute_units_[i].ocl_kernel.setArg(idx++, *((cl::Image *)outputs[i]->GetHandle().base));
     }
-
-    int n = DimsFunctionUtils::GetDim(input_dims, 0);
-    int c = DimsFunctionUtils::GetDim(input_dims, 1);
-    int h = DimsFunctionUtils::GetDim(input_dims, 2);
-    if (kernel_name_ == "NHC4W4ImageToCNH4Image") {
-        execute_units_[0].global_work_size = {static_cast<uint32_t>(UP_DIV(h, 4)), static_cast<uint32_t>(c * n)};
-        execute_units_[0].local_work_size  = {64, 1};
-    } else if (kernel_name_ == "CNH4ImageToNHC4W4Image") {
-        execute_units_[0].global_work_size = {static_cast<uint32_t>(UP_DIV(c, 4)), static_cast<uint32_t>(n * h)};
-        execute_units_[0].local_work_size  = {1, 64};
-    }
-
-    uint32_t idx = 0;
-    execute_units_[0].ocl_kernel.setArg(idx++, execute_units_[0].global_work_size[0]);
-    execute_units_[0].ocl_kernel.setArg(idx++, execute_units_[0].global_work_size[1]);
-    execute_units_[0].ocl_kernel.setArg(idx++, *((cl::Image *)inputs[0]->GetHandle().base));
-    execute_units_[0].ocl_kernel.setArg(idx++, n);
-    execute_units_[0].ocl_kernel.setArg(idx++, h);
-    execute_units_[0].ocl_kernel.setArg(idx++, *((cl::Image *)outputs[0]->GetHandle().base));
 
     return TNN_OK;
 }
