f01ae9aa2568e17edf2399b376799efe874b7276
jacinhu <jacinhu@tencent.com>
2021-09-09 19:38:33 +0800

    [CPU]fix cpu matmul crash again

--- source/tnn/device/cpu/acc/cpu_mat_mul_layer_acc.cc
+++ source/tnn/device/cpu/acc/cpu_mat_mul_layer_acc.cc
@@ -35,9 +35,9 @@ Status CpuMatMulLayerAcc::Init(Context *context, LayerParam *param, LayerResourc
             const std::vector<Blob *> &outputs){
     auto status = CpuLayerAcc::Init(context, param, resource, inputs, outputs);
 
-    auto layer_res = dynamic_cast<MatMulLayerResource *>(resource);
-    if (layer_res == NULL)
+    if (inputs.size() == 2)
         return TNN_OK;
+    auto layer_res = dynamic_cast<MatMulLayerResource *>(resource);
 
     const int data_size = layer_res->weight.GetDataCount();
     std::shared_ptr<float> weight(new float[data_size], [](float *p) { delete[] p; });
