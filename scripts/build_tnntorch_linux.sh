80f467127977a2cc2cce8995d2c74f6c8e71e974
neiltian-tencent <neiltian@tencent.com>
2021-10-29 11:17:52 +0000

    update tnntorch linux

--- scripts/build_tnntorch_linux.sh
+++ scripts/build_tnntorch_linux.sh
@@ -52,36 +52,35 @@ mkdir -p ${TNN_INSTALL_DIR}/lib
 echo $CUDA_TOOLKIT_ROOT_DIR
 
 cp -r ${TNN_ROOT_PATH}/include ${TNN_INSTALL_DIR}/
-cp libTNN.so* ${TNN_INSTALL_DIR}/lib/
-cp _pytnn.*.so ${TNN_INSTALL_DIR}/lib/ 
-cp test/TNNTest ${TNN_INSTALL_DIR}/lib/
+cp -d libTNN.so* ${TNN_INSTALL_DIR}/lib/
+cp -d _pytnn.*.so ${TNN_INSTALL_DIR}/lib/ 
 
 # deps
-cp /usr/local/cuda/lib64/libcudart.so* ${TNN_INSTALL_DIR}/lib/
-cp /usr/local/cuda/targets/x86_64-linux/lib/libnvrtc.so* ${TNN_INSTALL_DIR}/lib/
-cp /usr/local/cuda/targets/x86_64-linux/lib/libnvToolsExt*.so* ${TNN_INSTALL_DIR}/lib/
-cp /usr/lib/x86_64-linux-gnu/libcuda.so* ${TNN_INSTALL_DIR}/lib/
-cp /usr/lib/x86_64-linux-gnu/libcublas*.so* ${TNN_INSTALL_DIR}/lib/
-cp $TENSORRT_ROOT_DIR/lib/libnvinfer.so* ${TNN_INSTALL_DIR}/lib/
-cp $TENSORRT_ROOT_DIR/lib/libnvinfer_plugin.so* ${TNN_INSTALL_DIR}/lib/
-cp $TENSORRT_ROOT_DIR/lib/libmyelin.so* ${TNN_INSTALL_DIR}/lib/
-cp $CUDNN_ROOT_DIR/lib64/libcudnn.so* ${TNN_INSTALL_DIR}/lib/
-cp $CUDNN_ROOT_DIR/lib64/libcudnn_cnn_infer.so* ${TNN_INSTALL_DIR}/lib/
-cp $CUDNN_ROOT_DIR/lib64/libcudnn_ops_infer.so* ${TNN_INSTALL_DIR}/lib/
+cp -d /usr/local/cuda/lib64/libcudart.so* ${TNN_INSTALL_DIR}/lib/
+cp -d /usr/local/cuda/targets/x86_64-linux/lib/libnvrtc.so* ${TNN_INSTALL_DIR}/lib/
+cp -d /usr/local/cuda/targets/x86_64-linux/lib/libnvToolsExt*.so* ${TNN_INSTALL_DIR}/lib/
+cp -d /usr/lib/x86_64-linux-gnu/libcuda.so.* ${TNN_INSTALL_DIR}/lib/
+cp -d /usr/lib/x86_64-linux-gnu/libcublas*.so* ${TNN_INSTALL_DIR}/lib/
+cp -d $TENSORRT_ROOT_DIR/lib/libnvinfer.so* ${TNN_INSTALL_DIR}/lib/
+cp -d $TENSORRT_ROOT_DIR/lib/libnvinfer_plugin.so* ${TNN_INSTALL_DIR}/lib/
+cp -d $TENSORRT_ROOT_DIR/lib/libmyelin.so* ${TNN_INSTALL_DIR}/lib/
+cp -d $CUDNN_ROOT_DIR/lib64/libcudnn.so* ${TNN_INSTALL_DIR}/lib/
+cp -d $CUDNN_ROOT_DIR/lib64/libcudnn_cnn_infer.so* ${TNN_INSTALL_DIR}/lib/
+cp -d $CUDNN_ROOT_DIR/lib64/libcudnn_ops_infer.so* ${TNN_INSTALL_DIR}/lib/
 
 # torch
-cp ${LIBTORCH_ROOT_DIR}/lib/libtorch.so ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libtorch_cpu.so ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libtorch_cuda.so ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libc10.so ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libc10_cuda.so ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libgomp*.so* ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libnvToolsExt*.so* ${TNN_INSTALL_DIR}/lib/
-cp ${LIBTORCH_ROOT_DIR}/lib/libcudart-*.so* ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libtorch.so ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libtorch_cpu.so ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libtorch_cuda.so ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libc10.so ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libc10_cuda.so ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libgomp*.so* ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libnvToolsExt*.so* ${TNN_INSTALL_DIR}/lib/
+cp -d ${LIBTORCH_ROOT_DIR}/lib/libcudart-*.so* ${TNN_INSTALL_DIR}/lib/
 
 # torchvision libs
 if [ "$TORCHVISION_ENABLE" = "ON" ]; then
-    cp ${LIBTORCHVISION_ROOT_DIR}/lib/libtorchvision.so ${TNN_INSTALL_DIR}/lib/libtorchvision.so
+    cp -d ${LIBTORCHVISION_ROOT_DIR}/lib/libtorchvision.so ${TNN_INSTALL_DIR}/lib/libtorchvision.so
 fi
 
 cp ${TNN_ROOT_PATH}/source/pytnn/*.py ${TNN_INSTALL_DIR}/lib/
