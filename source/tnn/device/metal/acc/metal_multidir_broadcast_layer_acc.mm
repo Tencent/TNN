5f1595f9f334942ac239078ed41b276fbb6124e0
zhenfengli <zhenfengli@tencent.com>
2021-12-01 16:06:45 +0800

    1. solve binary bug

--- source/tnn/device/metal/acc/metal_multidir_broadcast_layer_acc.mm
+++ source/tnn/device/metal/acc/metal_multidir_broadcast_layer_acc.mm
@@ -92,16 +92,8 @@ Status MetalMultidirBroadcastLayerAcc::AllocateBufferParam(const std::vector<Blo
 
         if(dims_output.size()==5){
             metal_params.input_width  = DimsFunctionUtils::GetDim(dims_output, 3) * DimsFunctionUtils::GetDim(dims_output, 4);
-            <<<<<<< HEAD
             metal_params.real_input0_4 = DimsFunctionUtils::GetDim(dims_input0, 4);
             metal_params.real_input1_4 = DimsFunctionUtils::GetDim(dims_input1, 4);
-            =======
-
-            metal_params.real_input0_1 = DimsFunctionUtils::GetDim(dims_input0, 1);
-            metal_params.real_input0_2 = DimsFunctionUtils::GetDim(dims_input0, 2);
-            metal_params.real_input0_3 = DimsFunctionUtils::GetDim(dims_input0, 3);
-            metal_params.real_input0_4 = DimsFunctionUtils::GetDim(dims_input0, 4);
-            >>>>>>> dbe71efb1b8e892d7fe238383ae65ad76eaccede
         }else{
             metal_params.input_width  = DimsFunctionUtils::GetDim(dims_output, 3);
         }
@@ -120,26 +112,8 @@ Status MetalMultidirBroadcastLayerAcc::AllocateBufferParam(const std::vector<Blo
         metal_params.output_size   = metal_params.output_height * metal_params.output_width;
         metal_params.output_slice  = UP_DIV(dims_output[1], 4);
 
-        <<<<<<< HEAD
         metal_params.input0_size = DimsFunctionUtils::GetDimProduct(dims_input0, 2);
         metal_params.input1_size = DimsFunctionUtils::GetDimProduct(dims_input1, 2);
-        =======
-        metal_params.input0_size   =  DimsFunctionUtils::GetDimProduct(dims_input0, 2);
-        if (!(layer_res && buffer_weight_)) {
-            auto dims_input1  = inputs[1]->GetBlobDesc().dims;
-            metal_params.input1_size = DimsFunctionUtils::GetDimProduct(dims_input1, 2);
-
-            if(dims_input1.size()==5){
-                if ((dims_output[0]==dims_input1[0]) && (dims_output[1]==dims_input1[1]) && (dims_input1[2]==1) && (dims_input1[3]==1) && (dims_output[4]==dims_input1[4])) {
-                    layer_param->input1_broadcast_type = kBroadcastType5DimsHeightWidth;
-                }
-                metal_params.real_input1_1 = DimsFunctionUtils::GetDim(dims_input1, 1);
-                metal_params.real_input1_2 = DimsFunctionUtils::GetDim(dims_input1, 2);
-                metal_params.real_input1_3 = DimsFunctionUtils::GetDim(dims_input1, 3);
-                metal_params.real_input1_4 = DimsFunctionUtils::GetDim(dims_input1, 4);
-            }
-        }
-        >>>>>>> dbe71efb1b8e892d7fe238383ae65ad76eaccede
 
         metal_params.batch = dims_output[0];
 
