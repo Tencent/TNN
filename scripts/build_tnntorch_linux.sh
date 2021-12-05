7c3d1021df1014642e56de323a5d1a36202baf45
neiltian-tencent <neiltian@tencent.com>
2021-12-05 20:39:10 +0800

    [script] 减小安装包大小，并适配不同的cuda版本及cublas路径

--- scripts/build_tnntorch_linux.sh
+++ scripts/build_tnntorch_linux.sh
@@ -10,7 +10,7 @@ PYBIND_ENABLE="OFF"
 
 export CUDNN_ROOT_DIR=/usr/local/cudnn-8.1.1
 export TENSORRT_ROOT_DIR=/usr/local/TensorRT-7.2.3.4
-if [ -z $1 ];then export LIBTORCH_ROOT_DIR=/usr/local/libtorch-shared-1.8.1+cu102
+if [ -z $1 ];then export LIBTORCH_ROOT_DIR=`find /usr/local/ -name "libtorch-shared-1.8.1+*"`
 else
     export LIBTORCH_ROOT_DIR=$1
 fi
@@ -64,23 +64,22 @@ cp -d libTNN.so* ${TNN_INSTALL_DIR}/lib/
 cp -d /usr/local/cuda/lib64/libcudart.so* ${TNN_INSTALL_DIR}/lib/
 cp -d /usr/local/cuda/targets/x86_64-linux/lib/libnvrtc.so* ${TNN_INSTALL_DIR}/lib/
 cp -d /usr/local/cuda/targets/x86_64-linux/lib/libnvToolsExt*.so* ${TNN_INSTALL_DIR}/lib/
-cp -d /usr/lib/x86_64-linux-gnu/libcublas*.so* ${TNN_INSTALL_DIR}/lib/
-cp -d $TENSORRT_ROOT_DIR/lib/libnvinfer.so* ${TNN_INSTALL_DIR}/lib/
-cp -d $TENSORRT_ROOT_DIR/lib/libnvinfer_plugin.so* ${TNN_INSTALL_DIR}/lib/
-cp -d $TENSORRT_ROOT_DIR/lib/libmyelin.so* ${TNN_INSTALL_DIR}/lib/
-cp -d $CUDNN_ROOT_DIR/lib64/libcudnn.so* ${TNN_INSTALL_DIR}/lib/
-cp -d $CUDNN_ROOT_DIR/lib64/libcudnn_cnn_infer.so* ${TNN_INSTALL_DIR}/lib/
-cp -d $CUDNN_ROOT_DIR/lib64/libcudnn_ops_infer.so* ${TNN_INSTALL_DIR}/lib/
+
+#cublas
+cublas_dep_list=$( ldd libTNN.so | awk '{if (match($3, "cublas")){ print $3}}' )
+cp $cublas_dep_list ${TNN_INSTALL_DIR}/lib/
+
+#tensorrt
+tensorrt_dep_list=$( ldd libTNN.so | awk '{if (match($3, "TensorRT")){ print $3}}' )
+cp ${tensorrt_dep_list} ${TNN_INSTALL_DIR}/lib/
+
+#cudnn
+cudnn_dep_list=$( ldd libTNN.so | awk '{if (match($3, "cudnn")){ print $3}}' )
+cp $cudnn_dep_list ${TNN_INSTALL_DIR}/lib/
 
 # torch
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libtorch.so ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libtorch_cpu.so ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libtorch_cuda.so ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libc10.so ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libc10_cuda.so ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libgomp*.so* ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libnvToolsExt*.so* ${TNN_INSTALL_DIR}/lib/
-cp -d ${LIBTORCH_ROOT_DIR}/lib/libcudart-*.so* ${TNN_INSTALL_DIR}/lib/
+torch_dep_list=$( ldd libTNN.so | awk '{if (match($3,"libtorch-shared")){ print $3}}' )
+cp $torch_dep_list ${TNN_INSTALL_DIR}/lib/
 
 # torchvision libs
 if [ "$TORCHVISION_ENABLE" = "ON" ]; then
