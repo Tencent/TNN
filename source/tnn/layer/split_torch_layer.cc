200ddb7f6ed715648108e69814ec99eafff00e35
ealinli <ealinli@tencent.com>
2021-12-05 19:56:47 +0800

    remove debug code and clang format

--- source/tnn/layer/split_torch_layer.cc
+++ source/tnn/layer/split_torch_layer.cc
@@ -18,66 +18,66 @@
 
 namespace TNN_NS {
 
-    DECLARE_LAYER(SplitTorch, LAYER_SPLITTORCH);
+DECLARE_LAYER(SplitTorch, LAYER_SPLITTORCH);
 
-    Status SplitTorchLayer::InferOutputDataType() {
-        return BaseLayer::InferOutputDataType();
+Status SplitTorchLayer::InferOutputDataType() {
+    return BaseLayer::InferOutputDataType();
+}
+
+Status SplitTorchLayer::InferOutputShape(bool ignore_error) {
+    BaseLayer::InferOutputShape(ignore_error);
+
+    auto layer_param = dynamic_cast<SplitTorchLayerParam*>(param_);
+    if (!layer_param) {
+        return Status(TNNERR_PARAM_ERR, "SplitTorchLayer do not have valid param, please check node: " + layer_name_);
+    }
+    Blob* input_blob     = input_blobs_[0];
+    auto input_dims      = input_blob->GetBlobDesc().dims;
+    const int split_size = layer_param->split_size;
+
+    if (layer_param->axis < 0) {
+        layer_param->axis += input_dims.size();
     }
 
-    Status SplitTorchLayer::InferOutputShape(bool ignore_error) {
-        BaseLayer::InferOutputShape(ignore_error);
-
-        auto layer_param = dynamic_cast<SplitTorchLayerParam*>(param_);
-        if (!layer_param) {
-            return Status(TNNERR_PARAM_ERR, "SplitTorchLayer do not have valid param, please check node: " + layer_name_);
-        }
-        Blob* input_blob = input_blobs_[0];
-        auto input_dims  = input_blob->GetBlobDesc().dims;
-        const int split_size = layer_param->split_size;
-
-        if (layer_param->axis < 0) {
-            layer_param->axis += input_dims.size();
-        }
-
-        if (split_size == 0) {
-            return Status(TNNERR_PARAM_ERR, "SplitTorchLayer has invalid param, split size is zero");
-        }
-
-        const int input_size = input_dims[layer_param->axis];
-        const int num_split = input_size / split_size;
-        const int last_size = input_size % split_size;
-
-        std::vector<int> slices(num_split, split_size);
-        if (last_size > 0) {
-            slices.push_back(last_size);
-        }
-
-        layer_param->slices = slices;
-
-        if (layer_param->slices.size() != output_blobs_.size()) {
-            const int obs = output_blobs_.size();
-            int x = 0;
-            return Status(TNNERR_PARAM_ERR, "SplitTorchLayer has invalid param, slices size != output blobs size ");
-        }
-
-        int size_sum = layer_param->slices[0];
-        for (int i = 1; i < layer_param->slices.size(); i++) {
-            size_sum += layer_param->slices[i];
-        }
-
-        if (size_sum != input_dims[layer_param->axis]) {
-            return Status(TNNERR_PARAM_ERR, "SplitTorchLayer has invalid slices");
-        }
-
-        for (size_t i = 0; i < output_blobs_.size(); i++) {
-            auto output_dims                     = input_dims;
-            output_dims[layer_param->axis]       = layer_param->slices[i];
-            output_blobs_[i]->GetBlobDesc().dims = output_dims;
-        }
-
-        return TNN_OK;
+    if (split_size == 0) {
+        return Status(TNNERR_PARAM_ERR, "SplitTorchLayer has invalid param, split size is zero");
     }
 
-    REGISTER_LAYER(SplitTorch, LAYER_SPLITTORCH);
+    const int input_size = input_dims[layer_param->axis];
+    const int num_split  = input_size / split_size;
+    const int last_size  = input_size % split_size;
+
+    std::vector<int> slices(num_split, split_size);
+    if (last_size > 0) {
+        slices.push_back(last_size);
+    }
+
+    layer_param->slices = slices;
+
+    if (layer_param->slices.size() != output_blobs_.size()) {
+        const int obs = output_blobs_.size();
+        int x         = 0;
+        return Status(TNNERR_PARAM_ERR, "SplitTorchLayer has invalid param, slices size != output blobs size ");
+    }
+
+    int size_sum = layer_param->slices[0];
+    for (int i = 1; i < layer_param->slices.size(); i++) {
+        size_sum += layer_param->slices[i];
+    }
+
+    if (size_sum != input_dims[layer_param->axis]) {
+        return Status(TNNERR_PARAM_ERR, "SplitTorchLayer has invalid slices");
+    }
+
+    for (size_t i = 0; i < output_blobs_.size(); i++) {
+        auto output_dims                     = input_dims;
+        output_dims[layer_param->axis]       = layer_param->slices[i];
+        output_blobs_[i]->GetBlobDesc().dims = output_dims;
+    }
+
+    return TNN_OK;
+}
+
+REGISTER_LAYER(SplitTorch, LAYER_SPLITTORCH);
 
 }  // namespace TNN_NS
--- source/tnn/network/torch/torch_convert.cc
+++ source/tnn/network/torch/torch_convert.cc
@@ -63,7 +63,7 @@ c10::intrusive_ptr<runtime::TNNEngine> ConvertBlockToInstance(partitioning::Segm
         if (kind.is_prim() && ctx->dealPrime(node)) {
             continue;
         }
-        std::cout << kind.toQualString() << std::endl;
+        // std::cout << kind.toQualString() << std::endl;
 
         ConvertNodeToLayer(node, net_structure, net_resource);
     }
@@ -90,15 +90,6 @@ c10::intrusive_ptr<runtime::TNNEngine> ConvertBlockToInstance(partitioning::Segm
         }
         net_structure->inputs_shape_map = max_inputs_shape_map;
         net_structure->input_data_type_map = inputs_type_map;
-
-        static int __cnt = 0;
-        const std::string root = "./";
-        const std::string model_name = "splt-" + std::to_string(__cnt++);
-        const std::string proto_path = root + model_name + ".tnnproto";
-        const std::string model_path = root + model_name + ".tnnmodel";
-        TNN_NS::ModelPacker model_packer(net_structure, net_resource);
-        Status status = model_packer.Pack(proto_path, model_path);
-
 
         instance_ptr->instance_->Init(ctx->get_interpreter(), min_inputs_shape_map, max_inputs_shape_map);
         instance_ptr->is_init_ = true;
