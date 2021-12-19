c3d92a0f141d6dcb42d9049345814dbc13e2dbb5
ealinli <ealinli@tencent.com>
2021-12-19 19:56:30 +0800

    add equal and topk

--- source/tnn/network/torch/torch_op_converter.cc
+++ source/tnn/network/torch/torch_op_converter.cc
@@ -346,6 +346,10 @@ public:
                 layer_info->type     = LAYER_GREATER;
                 layer_info->type_str = "Greater";
                 break;
+            case at::aten::eq:
+                layer_info->type     = LAYER_EQUAL;
+                layer_info->type_str = "Equal";
+                break;
             default:
                 LOGE("Unsupport layer type %s\n", node->kind().toUnqualString());
                 ASSERT(0);
@@ -1050,7 +1054,8 @@ public:
     // Currently, casting data to float is not supported.
     bool IsSupported(const torch::jit::Node *node) {
         const auto& inputs = node->inputs();
-        if (inputs[1]->type()->kind() == c10::TypeKind::ScalarTypeType) {
+        const auto& input1_type = inputs[1]->type()->kind();
+        if (input1_type == c10::TypeKind::ScalarTypeType || input1_type == c10::TypeKind::IntType) {
             const auto dtype = getValue<int>(inputs[1]);
             // "6" means float data
             return dtype == 6;
@@ -1400,6 +1405,38 @@ public:
     }
 };
 
+// func: topk(Tensor self, int k, int dim=-1, bool largest=True, bool sorted=True) -> (Tensor values, Tensor indices)
+class TopKTorchConverter : public TorchOpConverter {
+public:
+    Status Convert(const torch::jit::Node *node, NetStructure *net_structure, NetResource *net_resource) {
+        std::shared_ptr<LayerInfo> layer_info = std::make_shared<LayerInfo>();
+        layer_info->type                      = LAYER_TOPK;
+        layer_info->type_str                  = "TopK";
+        layer_info->name                      = node->output(0)->debugName();
+
+        const auto &inputs = node->inputs();
+
+        layer_info->inputs.push_back(node->inputs()[0]->debugName());
+        layer_info->outputs.push_back(node->outputs()[0]->debugName());
+        layer_info->outputs.push_back(node->outputs()[1]->debugName());
+
+        auto layer_param = std::make_shared<TopKLayerParam>();
+
+        layer_param->k       = static_cast<int>(getValue<int64_t>(inputs[1]));
+        layer_param->axis    = static_cast<int>(getValue<int64_t>(inputs[2]));
+        layer_param->largest = static_cast<int>(getValue<bool>(inputs[3]));
+        layer_param->sorted  = static_cast<int>(getValue<bool>(inputs[4]));
+
+        layer_info->param = layer_param;
+
+        ADD_INPUTS_AND_OUTPUTS;
+
+        net_structure->layers.push_back(layer_info);
+
+        return TNN_OK;
+    }
+};
+
 class ListTorchConverter : public TorchOpConverter {
 public:
     bool IsSupported(const torch::jit::Node *node) {
@@ -1535,6 +1572,7 @@ REGISTER_TORCH_OP_CONVERTER(Binary, aten, add_)
 REGISTER_TORCH_OP_CONVERTER(Binary, aten, add)
 REGISTER_TORCH_OP_CONVERTER(Binary, aten, mul)
 REGISTER_TORCH_OP_CONVERTER(Binary, aten, div)
+REGISTER_TORCH_OP_CONVERTER(Binary, aten, eq)
 REGISTER_TORCH_OP_CONVERTER(Binary, aten, gt)
 REGISTER_TORCH_OP_CONVERTER(Clip, aten, clamp)
 REGISTER_TORCH_OP_CONVERTER(Concat, aten, cat)
@@ -1559,11 +1597,13 @@ REGISTER_TORCH_OP_CONVERTER(Relu, aten, relu_)
 REGISTER_TORCH_OP_CONVERTER(Reshape, aten, reshape)
 REGISTER_TORCH_OP_CONVERTER(Reshape, aten, view)
 REGISTER_TORCH_OP_CONVERTER(Sigmoid, aten, sigmoid)
+REGISTER_TORCH_OP_CONVERTER(Sigmoid, aten, sigmoid_)
 REGISTER_TORCH_OP_CONVERTER(Size, aten, size)
 REGISTER_TORCH_OP_CONVERTER(Softmax, aten, softmax)
 REGISTER_TORCH_OP_CONVERTER(Split, aten, split)
 REGISTER_TORCH_OP_CONVERTER(StridedSlice, aten, slice)
 REGISTER_TORCH_OP_CONVERTER(To, aten, to)
+REGISTER_TORCH_OP_CONVERTER(TopK, aten, topk)
 REGISTER_TORCH_OP_CONVERTER(Transpose, aten, transpose)
 // REGISTER_TORCH_OP_CONVERTER(Upsample, aten, upsample_bilinear2d)
 REGISTER_TORCH_OP_CONVERTER(Upsample, aten, upsample_nearest2d)
