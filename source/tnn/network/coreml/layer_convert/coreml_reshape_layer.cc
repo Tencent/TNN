63422fd398e83babf56155cae8273a1eb2331219
darrenyao <darrenyao@tencent.com>
2021-12-20 11:50:41 +0800

    [BUG]support shape input for coreml reshape layer

--- source/tnn/network/coreml/layer_convert/coreml_reshape_layer.cc
+++ source/tnn/network/coreml/layer_convert/coreml_reshape_layer.cc
@@ -32,7 +32,12 @@ DECLARE_COREML_LAYER_WITH_FUNC_DATA(Reshape, LAYER_RESHAPE,
 
 Status CoreMLReshapeLayer::BuildLayerType() {
     //layer type
-    coreml_layer_->layer_case = CORE_ML__SPECIFICATION__NEURAL_NETWORK_LAYER__LAYER_RANK_PRESERVING_RESHAPE;
+    if (layer_info_ && layer_info_->inputs.size() >= 2) {
+        coreml_layer_->layer_case = CORE_ML__SPECIFICATION__NEURAL_NETWORK_LAYER__LAYER_RESHAPE_DYNAMIC;
+    } else {
+        coreml_layer_->layer_case = CORE_ML__SPECIFICATION__NEURAL_NETWORK_LAYER__LAYER_RANK_PRESERVING_RESHAPE;
+    }
+    
     return TNN_OK;
 }
 
@@ -50,66 +55,74 @@ Status CoreMLReshapeLayer::BuildLayerParam() {
         }
     }
     
-    //layer param
-    auto param = layer_info_->param.get();
-    auto reshape_param = dynamic_cast<ReshapeLayerParam *>(param);
-    CHECK_PARAM_NULL(reshape_param);
-    auto input_size = layer_info_->inputs.size();
-    auto output_size = layer_info_->outputs.size();
-    auto shape_size = reshape_param->shape.size();
-    auto shape = reshape_param->shape;
-    if (input_shape_size <= 0 || output_shape_size <= 0 || shape_size != output_shape_size) {
-        return Status(TNNERR_MODEL_ERR, "CoreMLReshapeLayer has invalid input shape, output shape, or ReshapeLayerParam");
-    }
-    
-    // add unsqueeze to expenad dims
-    int reshape_size = MAX(output_shape_size, input_shape_size);
-    if (output_shape_size  > input_shape_size) {
-        RETURN_ON_NEQ(BuildUnsqueezeLayer(), TNN_OK);
-    }
-    
-    coreml_layer_param_ = std::shared_ptr<CoreML__Specification__RankPreservingReshapeLayerParams>(new CoreML__Specification__RankPreservingReshapeLayerParams);
-    coreml_layer_->rankpreservingreshape = (CoreML__Specification__RankPreservingReshapeLayerParams *)coreml_layer_param_.get();
-    core_ml__specification__rank_preserving_reshape_layer_params__init(coreml_layer_->rankpreservingreshape);
-    coreml_layer_->rankpreservingreshape->n_targetshape = reshape_size;
-    coreml_layer_shape_ = std::shared_ptr<int64_t>(new int64_t [coreml_layer_->rankpreservingreshape->n_targetshape], [](int64_t* p) { delete[] p; });
-    coreml_layer_->rankpreservingreshape->targetshape = (int64_t *)coreml_layer_shape_.get();
-    if(output_shape_size  < input_shape_size){
-        auto reduce_dims = input_shape_size - output_shape_size;
-        for(int i=0;i<reduce_dims;i++){
-            shape.push_back(1);
+    //reshape mode dynamic
+    if (layer_info_ && layer_info_->inputs.size() >= 2) {
+        coreml_layer_param_ = std::shared_ptr<CoreML__Specification__ReshapeDynamicLayerParams>(new CoreML__Specification__ReshapeDynamicLayerParams);
+        coreml_layer_->reshapedynamic = (CoreML__Specification__ReshapeDynamicLayerParams *)coreml_layer_param_.get();
+        core_ml__specification__reshape_dynamic_layer_params__init(coreml_layer_->reshapedynamic);
+    } else {
+        //reshape mode default
+        //layer param
+        auto param = layer_info_->param.get();
+        auto reshape_param = dynamic_cast<ReshapeLayerParam *>(param);
+        CHECK_PARAM_NULL(reshape_param);
+        auto input_size = layer_info_->inputs.size();
+        auto output_size = layer_info_->outputs.size();
+        auto shape_size = reshape_param->shape.size();
+        auto shape = reshape_param->shape;
+        if (input_shape_size <= 0 || output_shape_size <= 0 || shape_size != output_shape_size) {
+            return Status(TNNERR_MODEL_ERR, "CoreMLReshapeLayer has invalid input shape, output shape, or ReshapeLayerParam");
         }
-    }
-    for(int i=0; i<reshape_size; i++){
-        coreml_layer_->rankpreservingreshape->targetshape[i] = shape[i];
-    }
-    
-    //input & output rank must be equal!
-    //set inputtensor rank
-    coreml_layer_->n_inputtensor = input_size;
-    coreml_layer_inputtensor_arr_ = std::shared_ptr<CoreML__Specification__Tensor*>(new CoreML__Specification__Tensor* [input_size], [](CoreML__Specification__Tensor** p) { delete[] p; });
-    coreml_layer_->inputtensor = coreml_layer_inputtensor_arr_.get();
-    for(int i=0; i<input_size; i++){
-        coreml_layer_inputtensor_.push_back(std::shared_ptr<CoreML__Specification__Tensor>(new CoreML__Specification__Tensor));
-        coreml_layer_->inputtensor[i] = coreml_layer_inputtensor_[i].get();
-        core_ml__specification__tensor__init(coreml_layer_->inputtensor[i]);
-        coreml_layer_->inputtensor[i]->rank = (uint32_t)reshape_size;
-    }
-    
-    //set outputtensor rank
-    coreml_layer_->n_outputtensor = output_size;
-    coreml_layer_outputtensor_arr_ = std::shared_ptr<CoreML__Specification__Tensor*>(new CoreML__Specification__Tensor* [output_size], [](CoreML__Specification__Tensor** p) { delete[] p; });
-    coreml_layer_->outputtensor = coreml_layer_outputtensor_arr_.get();
-    for(int i=0; i<output_size; i++){
-        coreml_layer_outputtensor_.push_back(std::shared_ptr<CoreML__Specification__Tensor>(new CoreML__Specification__Tensor));
-        coreml_layer_->outputtensor[i] = coreml_layer_outputtensor_[i].get();
-        core_ml__specification__tensor__init(coreml_layer_->outputtensor[i]);
-        coreml_layer_->outputtensor[i]->rank = (uint32_t)reshape_size;
-    }
-    
-    // add squeeze to reduce dims
-    if (output_shape_size  < input_shape_size) {
-        RETURN_ON_NEQ(BuildSqueezeLayer(), TNN_OK);
+        
+        // add unsqueeze to expenad dims
+        int reshape_size = MAX(output_shape_size, input_shape_size);
+        if (output_shape_size  > input_shape_size) {
+            RETURN_ON_NEQ(BuildUnsqueezeLayer(), TNN_OK);
+        }
+        
+        coreml_layer_param_ = std::shared_ptr<CoreML__Specification__RankPreservingReshapeLayerParams>(new CoreML__Specification__RankPreservingReshapeLayerParams);
+        coreml_layer_->rankpreservingreshape = (CoreML__Specification__RankPreservingReshapeLayerParams *)coreml_layer_param_.get();
+        core_ml__specification__rank_preserving_reshape_layer_params__init(coreml_layer_->rankpreservingreshape);
+        coreml_layer_->rankpreservingreshape->n_targetshape = reshape_size;
+        coreml_layer_shape_ = std::shared_ptr<int64_t>(new int64_t [coreml_layer_->rankpreservingreshape->n_targetshape], [](int64_t* p) { delete[] p; });
+        coreml_layer_->rankpreservingreshape->targetshape = (int64_t *)coreml_layer_shape_.get();
+        if(output_shape_size  < input_shape_size){
+            auto reduce_dims = input_shape_size - output_shape_size;
+            for(int i=0;i<reduce_dims;i++){
+                shape.push_back(1);
+            }
+        }
+        for(int i=0; i<reshape_size; i++){
+            coreml_layer_->rankpreservingreshape->targetshape[i] = shape[i];
+        }
+        
+        //input & output rank must be equal!
+        //set inputtensor rank
+        coreml_layer_->n_inputtensor = input_size;
+        coreml_layer_inputtensor_arr_ = std::shared_ptr<CoreML__Specification__Tensor*>(new CoreML__Specification__Tensor* [input_size], [](CoreML__Specification__Tensor** p) { delete[] p; });
+        coreml_layer_->inputtensor = coreml_layer_inputtensor_arr_.get();
+        for(int i=0; i<input_size; i++){
+            coreml_layer_inputtensor_.push_back(std::shared_ptr<CoreML__Specification__Tensor>(new CoreML__Specification__Tensor));
+            coreml_layer_->inputtensor[i] = coreml_layer_inputtensor_[i].get();
+            core_ml__specification__tensor__init(coreml_layer_->inputtensor[i]);
+            coreml_layer_->inputtensor[i]->rank = (uint32_t)reshape_size;
+        }
+        
+        //set outputtensor rank
+        coreml_layer_->n_outputtensor = output_size;
+        coreml_layer_outputtensor_arr_ = std::shared_ptr<CoreML__Specification__Tensor*>(new CoreML__Specification__Tensor* [output_size], [](CoreML__Specification__Tensor** p) { delete[] p; });
+        coreml_layer_->outputtensor = coreml_layer_outputtensor_arr_.get();
+        for(int i=0; i<output_size; i++){
+            coreml_layer_outputtensor_.push_back(std::shared_ptr<CoreML__Specification__Tensor>(new CoreML__Specification__Tensor));
+            coreml_layer_->outputtensor[i] = coreml_layer_outputtensor_[i].get();
+            core_ml__specification__tensor__init(coreml_layer_->outputtensor[i]);
+            coreml_layer_->outputtensor[i]->rank = (uint32_t)reshape_size;
+        }
+        
+        // add squeeze to reduce dims
+        if (output_shape_size  < input_shape_size) {
+            RETURN_ON_NEQ(BuildSqueezeLayer(), TNN_OK);
+        }
     }
     
     return TNN_OK;
