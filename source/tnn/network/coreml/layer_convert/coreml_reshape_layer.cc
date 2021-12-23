074dce7b5c250436b9479e2f54b76cb5e12d8049
teslawho <597645882@qq.com>
2021-12-23 17:47:27 +0800

    [BUG] fix reshape layer when reshape_type=1 (input_shape_size = output_shape_size = 4)

--- source/tnn/network/coreml/layer_convert/coreml_reshape_layer.cc
+++ source/tnn/network/coreml/layer_convert/coreml_reshape_layer.cc
@@ -85,7 +85,7 @@ Status CoreMLReshapeLayer::BuildLayerParam() {
         
         // add permute to convert nchw to nhwc, when reshape_type = 1
         if (reshape_type == 1) {
-            if (input_shape_size != output_shape_size) {
+            if ((input_shape_size!=4) || (output_shape_size!=4)) {
                 return Status(TNNERR_MODEL_ERR, "CoreMLReshapeLayer input rank must be equal to output rank, when reshape_type = 1");
             }
             RETURN_ON_NEQ(BuildPermute0Layer(), TNN_OK);
