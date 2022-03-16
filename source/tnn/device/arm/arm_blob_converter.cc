e364b835ccd8b378c4a177e30b66facacad049a4
saner zheng <zqawszqaws@126.com>
2022-03-16 14:26:50 +0800

    Bugfix from train branch (#1592)
    * [BUG] fix get dims value bug when input is 1D or 2D in arm_reduce_layer_acc.cc.
    * [BUG] fix Convert from NCHW to NHWC error when input is on arm device.
    * [BUG] fix convert mat to blob bug when input is NC_INT32 on arm device.
    * [BUG] fix tflite_converter bug when transform a activation layer.
    * add nchw format condition when copy int32 mat to blob
    * rollback changes on tflite_op_converter.cc
    
    Co-authored-by: sanerzheng <sanerzheng@tencent.com>

--- source/tnn/device/arm/arm_blob_converter.cc
+++ source/tnn/device/arm/arm_blob_converter.cc
@@ -191,7 +191,7 @@ Status ArmBlobConverterAcc::ConvertFromMatAsync(Mat& image, MatConvertParam para
                 fused_int8_bias[i]  = 0;
             }
         }
-    } else if (desc.data_type == DATA_TYPE_INT32) {
+    } else if (desc.data_type == DATA_TYPE_INT32 && desc.data_format == DATA_FORMAT_NCHW) {
         int count = DimsVectorUtils::Count(blob_->GetBlobDesc().dims);
         int ele_size = DataTypeUtils::GetBytesSize(desc.data_type);
         if (image.GetMatType() == NC_INT32) {
