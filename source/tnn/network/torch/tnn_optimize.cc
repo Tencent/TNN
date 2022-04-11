b77f4ec74c9916caea46ad2953bed2e7fdd0ed1a
bohebuleng <704391300@qq.com>
2022-04-11 18:06:05 +0800

    fix index overstep the boundary

--- source/tnn/network/torch/tnn_optimize.cc
+++ source/tnn/network/torch/tnn_optimize.cc
@@ -41,46 +41,49 @@ void RemoveSingleConcat(NetStructure* net_structure, NetResource* net_resource) 
 
 void RemoveNormClampminExpandasDiv(NetStructure* net_structure, NetResource* net_resource) {
     auto& layers              = net_structure->layers;
+    const int count           = (const int)layers.size();
     std::vector<std::shared_ptr<LayerInfo>> layers_fused;
 
     // Normalize <= Norm - Clampmin - Expandas - Div
-    for (auto iter = layers.begin(); iter != layers.end(); iter++) {
-        auto& norm_layer = *iter;
-        if (norm_layer->type != TNN_NS::LAYER_NORM){
+    for (int index=0; index < count; index++) {
+        auto& norm_layer = layers[index];
+        if (index < count - 3) {
+            if (norm_layer->type != TNN_NS::LAYER_NORM){
+                layers_fused.push_back(norm_layer);
+                continue;
+            }
+            
+            auto clampmin_layer = layers[index+1];
+            auto expandas_layer = layers[index+2];
+            auto div_layer      = layers[index+3];
+            
+            if (clampmin_layer->type != TNN_NS::LAYER_CLAMPMIN || expandas_layer->type != TNN_NS::LAYER_EXPANDAS || div_layer->type != TNN_NS::LAYER_DIV ||
+                expandas_layer->outputs[0] != div_layer->inputs[1]) {
+                layers_fused.push_back(norm_layer);
+                continue;
+            }
+
+            auto norm_param         = std::dynamic_pointer_cast<NormLayerParam>(norm_layer->param);
+            auto clampmin_param     = std::dynamic_pointer_cast<ClampminLayerParam>(clampmin_layer->param);
+            const auto dim           = norm_param->dim;
+            const auto p             = norm_param->p;
+            const auto min           = clampmin_param->min;
+
+            auto normalize_param     = new TNN_NS::NormalizeLayerParam;
+            normalize_param->epsilon = min;
+            normalize_param->axis    = dim;
+            normalize_param->p       = p;
+            norm_layer->param        = std::shared_ptr<TNN_NS::LayerParam>(normalize_param);
+            norm_layer->type         = TNN_NS::LAYER_NORMALIZE;
+            norm_layer->type_str     = "Normalize";
+
+            norm_layer->outputs.clear();
+            norm_layer->outputs = div_layer->outputs;
             layers_fused.push_back(norm_layer);
-            continue;
+            index = index + 3;
+        } else {
+            layers_fused.emplace_back(layers.at(index));
         }
-        
-        auto clampmin_iter = iter + 1;
-        auto expandas_iter = iter + 2;
-        auto div_iter      = iter + 3;
-        auto clampmin_layer = *clampmin_iter;
-        auto expandas_layer = *expandas_iter;
-        auto div_layer      = *div_iter;
-        
-        if (clampmin_layer->type != TNN_NS::LAYER_CLAMPMIN || expandas_layer->type != TNN_NS::LAYER_EXPANDAS || div_layer->type != TNN_NS::LAYER_DIV ||
-            expandas_layer->outputs[0] != div_layer->inputs[1]) {
-            continue;
-        }
-
-        auto norm_param         = std::dynamic_pointer_cast<NormLayerParam>(norm_layer->param);
-        auto clampmin_param     = std::dynamic_pointer_cast<ClampminLayerParam>(clampmin_layer->param);
-        const auto dim           = norm_param->dim;
-        const auto p             = norm_param->p;
-        const auto min           = clampmin_param->min;
-
-        auto normalize_param     = new TNN_NS::NormalizeLayerParam;
-        normalize_param->epsilon = min;
-        normalize_param->axis    = dim;
-        normalize_param->p       = p;
-        norm_layer->param        = std::shared_ptr<TNN_NS::LayerParam>(normalize_param);
-        norm_layer->type         = TNN_NS::LAYER_NORMALIZE;
-        norm_layer->type_str     = "Normalize";
-
-        norm_layer->outputs.clear();
-        norm_layer->outputs = div_layer->outputs;
-        layers_fused.push_back(norm_layer);
-        iter = iter + 3;
     }
     net_structure->layers = layers_fused;
 }
