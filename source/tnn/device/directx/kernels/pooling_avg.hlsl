4073b3a9267a455dee64a61c2b66527243451377
jacinhu <jacinhu@tencent.com>
2022-04-20 16:20:51 +0800

    fix pooling avg kernel

--- source/tnn/device/directx/kernels/pooling_avg.hlsl
+++ source/tnn/device/directx/kernels/pooling_avg.hlsl
@@ -12,6 +12,8 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
+#define UP_DIV(A, B) (((A) + (B) - 1) / B)
+
 Texture2D<float4> input: register(t0);
 RWTexture2D<float4> output: register(u0);
 
