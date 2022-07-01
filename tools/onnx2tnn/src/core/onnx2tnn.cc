35683b3e8d20ba6c711db8691520bcfd5f306b18
darrenyao <darrenyao@tencent.com>
2022-07-01 10:51:09 +0800

    [UPD]add convert logic of swish

--- tools/onnx2tnn/src/core/onnx2tnn.cc
+++ tools/onnx2tnn/src/core/onnx2tnn.cc
@@ -532,7 +532,8 @@ int Onnx2TNN::OnnxExtractBlobWeights() {
     // FuseShuffleChannel(mutable_graph, index_nodes, weights, node_reference, blob_names);
     FuseLogSigmoid(mutable_graph, index_nodes, weights, node_reference, blob_names);
     FuseSoftmax(mutable_graph, index_nodes, weights, node_reference, blob_names);
-    FuseSwish(mutable_graph, index_nodes, weights, node_reference, blob_names);
+    //right now we dont fuse swish, we should realize it on arm opencl metal cuda x86 later
+    //FuseSwish(mutable_graph, index_nodes, weights, node_reference, blob_names);
     FuseHardSigmoid(mutable_graph, index_nodes, weights, node_reference, blob_names);
     FuseHardSwish(mutable_graph, index_nodes, weights, node_reference, blob_names);
     FuseGELU(mutable_graph, index_nodes, weights, node_reference, blob_names);
