5c33a764aa3168af6c38fc8d7f1c6dea15401b73
lucasktian <lucasktian@tencent.com>
2022-08-05 10:54:57 +0800

    [ARM][UPD]1. 调整 pad_utils 的位置,并重命名为 pad_function;2. update tnn.xcodeproj; (#1753)

--- source/tnn/device/arm/acc/arm_pad_layer_acc.cc
+++ source/tnn/device/arm/acc/arm_pad_layer_acc.cc
@@ -14,8 +14,8 @@
 
 #include "tnn/device/arm/acc/Float4.h"
 #include "tnn/device/arm/acc/arm_layer_acc.h"
+#include "tnn/device/arm/acc/compute/pad_function.h"
 #include "tnn/utils/data_type_utils.h"
-#include "tnn/utils/pad_utils.h"
 
 namespace TNN_NS {
 
