41de201a5f26d19eb68f16a258010df1e0619e2f
lucasktian <lucasktian@tencent.com>
2023-01-05 11:51:23 +0800

    [ARM][UPD]1. fix bug for stride_slice_v2 (arm); 2. add unit test stride_slice_v2 layer;

---
+++ test/unit_test/layer_test/test_stride_slice_v2_layer.cc
@@ -0,0 +1,71 @@
+// Tencent is pleased to support the open source community by making TNN available.
+//
+// Copyright (C) 2020 THL A29 Limited, a Tencent company. All rights reserved.
+//
+// Licensed under the BSD 3-Clause License (the "License"); you may not use this file except
+// in compliance with the License. You may obtain a copy of the License at
+//
+// https://opensource.org/licenses/BSD-3-Clause
+//
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+#include "test/unit_test/layer_test/layer_test.h"
+#include "test/unit_test/unit_test_common.h"
+#include "test/unit_test/utils/network_helpers.h"
+#include "tnn/utils/dims_utils.h"
+
+namespace TNN_NS {
+
+class StrideSliceV2LayerTest
+    : public LayerTest,
+      public ::testing::WithParamInterface<std::tuple<int, int, int, int, int, int, int, int, DataType>> {};
+
+INSTANTIATE_TEST_SUITE_P(LayerTest, StrideSliceV2LayerTest,
+                         ::testing::Combine(testing::Values(1, 2),
+                                            // channel
+                                            testing::Values(3, 4, 7, 8, 15),
+                                            // height
+                                            testing::Values(3, 6),
+                                            // width
+                                            testing::Values(3, 7),
+                                            // start
+                                            testing::Values(0, 1),
+                                            // end
+                                            testing::Values(3, 4, 5, 8, 15),
+                                            // axis
+                                            testing::Values(1, 2, 3),
+                                            // step
+                                            testing::Values(1, 2), testing::Values(DATA_TYPE_FLOAT, DATA_TYPE_HALF)));
+
+TEST_P(StrideSliceV2LayerTest, StrideSliceV2Layer) {
+    // get param
+    int batch          = std::get<0>(GetParam());
+    int channel        = std::get<1>(GetParam());
+    int height         = std::get<2>(GetParam());
+    int width          = std::get<3>(GetParam());
+    int start          = std::get<4>(GetParam());
+    int end            = std::get<5>(GetParam());
+    int axis           = std::get<6>(GetParam());
+    int step           = std::get<7>(GetParam());
+    DataType data_type = std::get<8>(GetParam());
+
+    DeviceType dev        = ConvertDeviceType(FLAGS_dt);
+    Precision precision   = SetPrecision(dev, data_type);
+    DimsVector input_dims = {batch, channel, height, width};
+    // param
+    std::shared_ptr<StrideSliceV2LayerParam> param(new StrideSliceV2LayerParam());
+    param->name    = "StrideSliceV2";
+    param->begins  = {start};
+    param->ends    = {end};
+    param->axes    = {axis};
+    param->strides = {step};
+    // generate interpreter
+    auto interpreter = GenerateInterpreter("StridedSliceV2", {input_dims}, param);
+
+    Run(interpreter, precision);
+}
+
+}  // namespace TNN_NS
