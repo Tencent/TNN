ed23edc14a44b6c513895d381924ab7a487d813b
doxu <doxu@tencent.com>
2024-12-19 15:40:31 +0800

    [TRT] Add CUDA 10- compatible codes for Shape Related TRT layer builders

--- source/tnn/network/tensorrt/layer_builder/expand_layer_builder.cc
+++ source/tnn/network/tensorrt/layer_builder/expand_layer_builder.cc
@@ -35,7 +35,11 @@ ILayer* ExpandTRTLayerBuilder::AddToNetwork(INetworkDefinition* network) {
 
     ITensor* inputDims;
     if (input_tensors[0]->getDimensions().nbDims != 0)
+        #if NV_TENSORRT_MAJOR * 10 + NV_TENSORRT_MINOR >= 100
         inputDims = network->addCast(*(network->addShape(*input_tensors[0])->getOutput(0)), nvinfer1::DataType::kINT32)->getOutput(0);
+        #else
+        inputDims = network->addShape(*input_tensors[0])->getOutput(0);
+        #endif
     int inputRank;
     if (input_tensors[0]->getDimensions().nbDims != 0) {
         inputRank = inputDims->getDimensions().d[0];
@@ -79,13 +83,23 @@ ILayer* ExpandTRTLayerBuilder::AddToNetwork(INetworkDefinition* network) {
             tmpWeight.values = input_data_tensor;
         }
         tmpWeight.count = 1;
-        auto tmp_tensor = network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0);
         if (input_tensors[0]->getDimensions().nbDims != 0) {
+            #if NV_TENSORRT_MAJOR * 10 + NV_TENSORRT_MINOR >= 100
+            auto int64_shape_tensor = network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0);
             nvinfer1::ITensor* const args[2] = {
-                network->addCast(*tmp_tensor, nvinfer1::DataType::kINT32)->getOutput(0), inputDims};
+                network->addCast(*int64_shape_tensor, nvinfer1::DataType::kINT32)->getOutput(0), inputDims};
+            #else
+            nvinfer1::ITensor* const args[2] = {
+                network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0), inputDims};
+            #endif
             newDims = network->addConcatenation(args, 2)->getOutput(0);
         } else {
-            newDims = network->addCast(*tmp_tensor, nvinfer1::DataType::kINT32)->getOutput(0);
+            #if NV_TENSORRT_MAJOR * 10 + NV_TENSORRT_MINOR >= 100
+            auto int64_shape_tensor = network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0);
+            newDims = network->addCast(*int64_shape_tensor, nvinfer1::DataType::kINT32)->getOutput(0);
+            #else
+            newDims = network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0);
+            #endif
         }
     } else {
         newDims = inputDims;
@@ -108,8 +122,14 @@ ILayer* ExpandTRTLayerBuilder::AddToNetwork(INetworkDefinition* network) {
         tmpWeight.type = nvinfer1::DataType::kINT32;
         tmpWeight.values = layer_param->shape.data();
         tmpWeight.count = 1;
+        #if NV_TENSORRT_MAJOR * 10 + NV_TENSORRT_MINOR >= 100
         nvinfer1::ITensor* const args[2] = {
-            network->addCast(*(network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0)), nvinfer1::DataType::kINT32)->getOutput(0), shape};
+            network->addCast(*(network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0)),
+                             nvinfer1::DataType::kINT32)->getOutput(0), shape};
+        #else
+        nvinfer1::ITensor* const args[2] = {
+            network->addShape(*network->addConstant(tmpDims, tmpWeight)->getOutput(0))->getOutput(0), shape};
+        #endif
         newShape = network->addConcatenation(args, 2)->getOutput(0);
     } else {
         newShape = shape;
@@ -135,7 +155,11 @@ ILayer* ExpandTRTLayerBuilder::AddToNetwork(INetworkDefinition* network) {
         tmpWeight.count = 1;
         ILayer* one_shape_constant_layer = network->addConstant(tmpDims, tmpWeight);
         one_shape_constant_layer->setName((layer_name_+"_one_shape_constant").c_str());
+        #if NV_TENSORRT_MAJOR * 10 + NV_TENSORRT_MINOR >= 100
         one = network->addCast(*(network->addShape(*one_shape_constant_layer->getOutput(0))->getOutput(0)), nvinfer1::DataType::kINT32)->getOutput(0);
+        #else
+        one = network->addShape(*one_shape_constant_layer->getOutput(0))->getOutput(0);
+        #endif
     }
 
     ITensor* strides = network->addElementWise(*one,
